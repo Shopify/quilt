51aeba1068acbcd3999f31bff2840c35e3185eab
Tzvi Melamed <tzvi@melamed.biz>
2018-04-18 12:52:07 -0400

    :pushpin: Ensure TS versions stay in sync

---
+++ test/typescript-version.test.ts
@@ -0,0 +1,40 @@
+import {readdirSync, readFileSync} from 'fs';
+import * as path from 'path';
+
+describe('typescript version', () => {
+  it('the version in each package matches the root version', () => {
+    const rootPackageJSON = require('../package.json');
+    const rootVersion = rootPackageJSON.devDependencies.typescript;
+
+    const packagesPath = path.resolve(__dirname, '..', 'packages');
+    const packageNames = readdirSync(packagesPath);
+
+    for (const packageName of packageNames) {
+      const packageJSONPath = path.join(
+        packagesPath,
+        packageName,
+        'package.json',
+      );
+
+      const packageJSON = JSON.parse(
+        readFileSync(packageJSONPath, {encoding: 'utf8'}),
+      );
+
+      expect(packageJSON).toMatchObject({
+        devDependencies: {
+          typescript: rootVersion,
+        },
+      });
+    }
+  });
+
+  it('the version in the plop file matches the root version', () => {
+    const rootPackageJSON = require('../package.json');
+    const rootVersion = rootPackageJSON.devDependencies.typescript;
+
+    const plopPackageJSON = require('../templates/package.hbs.json');
+    const plopVersion = plopPackageJSON.devDependencies.typescript;
+
+    expect(plopVersion).toBe(rootVersion);
+  });
+});
