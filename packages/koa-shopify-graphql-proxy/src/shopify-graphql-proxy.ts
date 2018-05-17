0874a09c74b1f0a8e44848a9756231e0fbcfd32f
Matt <matt.seccafien@shopify.com>
2018-05-17 14:07:08 -0400

    return middleware function from proxy pkg

--- packages/koa-shopify-graphql-proxy/src/shopify-graphql-proxy.ts
+++ packages/koa-shopify-graphql-proxy/src/shopify-graphql-proxy.ts
@@ -8,45 +8,47 @@ export interface SessionContext extends Context {
 export const PROXY_BASE_PATH = '/graphql';
 export const GRAPHQL_PATH = '/admin/api/graphql.json';
 
-export default async function shopifyGraphQLProxy(
-  ctx: SessionContext,
-  next: () => Promise<any>,
-) {
-  const {session = {}} = ctx;
-  const {accessToken, shop} = session;
+export default function shopifyGraphQLProxy() {
+  return async function shopifyGraphQLProxyMiddleware(
+    ctx: SessionContext,
+    next: () => Promise<any>,
+  ) {
+    const {session = {}} = ctx;
+    const {accessToken, shop} = session;
 
-  if (ctx.path !== PROXY_BASE_PATH || ctx.method !== 'POST') {
-    await next();
-    return;
-  }
+    if (ctx.path !== PROXY_BASE_PATH || ctx.method !== 'POST') {
+      await next();
+      return;
+    }
 
-  if (accessToken == null || shop == null) {
-    ctx.throw(403, 'Unauthorized');
-    return;
-  }
+    if (accessToken == null || shop == null) {
+      ctx.throw(403, 'Unauthorized');
+      return;
+    }
 
-  await proxy(shop, {
-    https: true,
-    parseReqBody: false,
-    // Setting request header here, not response. That's why we don't use ctx.set()
-    // proxy middleware will grab this request header
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Shopify-Access-Token': accessToken,
-    },
-    proxyReqPathResolver() {
-      return `https://${shop}${GRAPHQL_PATH}`;
-    },
-  })(
-    ctx,
+    await proxy(shop, {
+      https: true,
+      parseReqBody: false,
+      // Setting request header here, not response. That's why we don't use ctx.set()
+      // proxy middleware will grab this request header
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Shopify-Access-Token': accessToken,
+      },
+      proxyReqPathResolver() {
+        return `https://${shop}${GRAPHQL_PATH}`;
+      },
+    })(
+      ctx,
 
-    /*
-      We want this middleware to terminate, not fall through to the next in the chain,
-      but sadly it doesn't support not passing a `next` function. To get around this we
-      just pass our own dummy `next` that resolves immediately.
-    */
-    noop,
-  );
+      /*
+        We want this middleware to terminate, not fall through to the next in the chain,
+        but sadly it doesn't support not passing a `next` function. To get around this we
+        just pass our own dummy `next` that resolves immediately.
+      */
+      noop,
+    );
+  };
 }
 
 async function noop() {}
