13c6bb22464a449af2f37d32abfb46fe1cc88d33
Mallory Allen <mallory.allen@gmail.com>
2018-05-18 16:11:16 -0400

    ✨ create @shopify/react-shortcuts

---
+++ packages/react-shortcuts/src/Shortcut/Shortcut.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import {contextTypes} from '../ShortcutProvider';
+import {Key} from '../types';
+
+export interface Props {
+  keys: Key[];
+  node?: HTMLElement | null;
+  ignoreInput?: boolean;
+  onMatch(keys: Key[]): void;
+  allowDefault?: boolean;
+}
+
+export interface Subscription {
+  unsubscribe(): void;
+}
+
+export default class Shortcut extends React.Component<Props, never> {
+  static contextTypes = contextTypes;
+  public data = {
+    node: this.props.node,
+    keys: this.props.keys,
+    ignoreInput: this.props.ignoreInput || false,
+    onMatch: this.props.onMatch,
+    allowDefault: this.props.allowDefault,
+  };
+  public subscription!: Subscription;
+
+  componentDidMount() {
+    const {node} = this.data;
+
+    if (node != null) {
+      return;
+    }
+
+    const {shortcutManager} = this.context;
+    this.subscription = shortcutManager.subscribe(this.data);
+  }
+
+  componentWillUnmount() {
+    if (this.subscription == null) {
+      return;
+    }
+
+    this.subscription.unsubscribe();
+  }
+
+  render() {
+    return null;
+  }
+}
---
+++ packages/react-shortcuts/src/ShortcutProvider/index.ts
@@ -0,0 +1,4 @@
+import ShortcutProvider from './ShortcutProvider';
+
+export {Props, Context, contextTypes} from './ShortcutProvider';
+export default ShortcutProvider;
