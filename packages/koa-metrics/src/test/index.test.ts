959f243eadaac604dd05071918c692a10e5380a0
Tzvi Melamed <tzvi@melamed.biz>
2018-05-22 15:54:24 -0400

    :ok_hand: Mal lint

--- packages/koa-metrics/src/test/index.test.ts
+++ packages/koa-metrics/src/test/index.test.ts
@@ -1,5 +1,51 @@
 describe('koa-metrics', () => {
-  it('works', () => {
-    // write tests for your package here!
+  it.skip('provides a hot-shots client as ctx.state.statsd');
+
+  it.skip('uses the provided prefix for statsd metrics');
+
+  it.skip('uses the provided host for the statsd client');
+
+  describe('request_queuing_time', () => {
+    it.skip('logs the queuing time based on the X-Request-Start header');
+
+    it.skip(
+      'does not log the queuing time when the X-Request-Start header is not present',
+    );
+
+    it.skip(
+      'emits this metric when the request processing begins (i.e. before calling next())',
+    );
+
+    it.skip('tags the log with the path');
+
+    it.skip('tags the log with the request method');
+  });
+
+  describe('request_time', () => {
+    it.skip('logs the request time');
+
+    it.skip('tags the log with the path');
+
+    it.skip('tags the log with the request method');
+
+    it.skip('tags the log with the response code');
+
+    it.skip('tags the log with the response type');
+  });
+
+  describe('request_content_length', () => {
+    it.skip(
+      'logs the request content length based on the Content-Length header',
+    );
+
+    it.skip('logs undefined when the Content-Length header is not present');
+
+    it.skip('tags the log with the path');
+
+    it.skip('tags the log with the request method');
+
+    it.skip('tags the log with the response code');
+
+    it.skip('tags the log with the response type');
   });
 });
