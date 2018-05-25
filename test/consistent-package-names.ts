3681f058cd02bb6b38482bfe90078c84947a1c6c
Mallory Allen <mallory.allen@shopify.com>
2018-05-25 10:57:09 -0400

    ✅ test package name validity (#132)
    * ✅ test package name validity
    
    * 👌 check for existance before testing names match

---
+++ test/consistent-package-names.ts
@@ -0,0 +1,27 @@
+import {readdirSync, readFileSync, existsSync} from 'fs';
+import * as path from 'path';
+
+describe('package name', () => {
+  it('matches the path name', () => {
+    const packagesPath = path.resolve(__dirname, '..', 'packages');
+    const packageNames = readdirSync(packagesPath);
+
+    for (const packageName of packageNames) {
+      const packageJSONPath = path.join(
+        packagesPath,
+        packageName,
+        'package.json',
+      );
+
+      if (!existsSync(packageJSONPath)) {
+        continue;
+      }
+
+      const packageJSON = JSON.parse(
+        readFileSync(packageJSONPath, {encoding: 'utf8'}),
+      );
+
+      expect(packageJSON.name).toBe(packageName);
+    }
+  });
+});
