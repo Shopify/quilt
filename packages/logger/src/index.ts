0ef10d82ff5f8c2d22ea6d49c6248869a697797c
Tzvi Melamed <tzvi@melamed.biz>
2018-06-25 15:37:34 -0400

    :fire: removing dependencies

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,3 +1,13 @@
+import {Logger} from './Logger';
+import {ConsoleFormatter} from './formatters';
+
 export * from './Logger';
 export * from './formatters';
 export * from './types';
+
+const logger = new Logger({
+  name: 'fred',
+  formatter: new ConsoleFormatter(),
+});
+
+logger.error(new Error('foo'));
