be61285595e58a915297335efcdcbbf8c9552c71
Chris Sauve <chrismsauve@gmail.com>
2018-08-21 23:27:56 -0300

    Fix tests

--- test/setup.ts
+++ test/setup.ts
@@ -3,8 +3,6 @@ import 'isomorphic-fetch';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import './matchers';
-
 if (Intl.PluralRules == null) {
   require('intl-pluralrules');
 }
