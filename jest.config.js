9518adf83b4f67171c067451f0a8bbf178b9a5a1
Mallory Allen <mallory.allen@gmail.com>
2018-08-31 12:20:58 -0400

    ✨ run jest without needing to build first

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+const {readdirSync, existsSync} = require('fs');
+const path = require('path');
+
+const moduleNameMapper = getPackageNames().reduce((accumulator, name) => {
+  const scopedName = `@shopify/${name}`;
+  accumulator[scopedName] = `<rootDir>/packages/${name}/src/index.ts`;
+  return accumulator;
+}, {});
+
+module.exports = {
+  setupFiles: ['./test/setup.ts'],
+  setupTestFrameworkScriptFile: './test/each-test.ts',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '\\.(gql|graphql)$': 'jest-transform-graphql',
+  },
+  testRegex: '.*\\.test\\.tsx?$',
+  testEnvironmentOptions: {
+    url: 'http://localhost:3000/',
+  },
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
+  moduleNameMapper,
+};
+
+function getPackageNames() {
+  const packagesPath = path.join(__dirname, 'packages');
+  return readdirSync(packagesPath).filter(packageName => {
+    const packageJSONPath = path.join(
+      packagesPath,
+      packageName,
+      'package.json',
+    );
+    return existsSync(packageJSONPath);
+  });
+}
