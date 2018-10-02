a88a5097052bed5ffc9c19d16e4f1e211e486be2
KY <infcurious@gmail.com>
2018-10-03 04:20:57 +0800

    Setup size-limit to keep the packages tiny

---
+++ .size-limit.js
@@ -0,0 +1,132 @@
+module.exports = [
+  {
+    "webpack": false,
+    "path": ["packages/koa-liveness-ping/dist"],
+    "limit": "1.25KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/jest-dom-mocks/dist"],
+    "limit": "6.27KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/admin-graphql-api-utilities/dist"],
+    "limit": "600B"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/koa-shopify-auth/dist"],
+    "limit": "14.1KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/dates/dist"],
+    "limit": "9.6KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/logger/dist"],
+    "limit": "3.4KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-import-remote/dist"],
+    "limit": "3.1KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/jest-mock-apollo/dist"],
+    "limit": "4.9KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/jest-koa-mocks/dist"],
+    "limit": "3.3KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/jest-mock-router/dist"],
+    "limit": "1.1KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-shortcuts/dist"],
+    "limit": "8.2KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-preconnect/dist"],
+    "limit": "850B"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-compose/dist"],
+    "limit": "660B"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-html/dist"],
+    "limit": "3.4KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/with-env/dist"],
+    "limit": "410B"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-shopify-app-route-propagator/dist"],
+    "limit": "1.7KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/koa-shopify-graphql-proxy/dist"],
+    "limit": "2.4KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/enzyme-utilities/dist"],
+    "limit": "3.1KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/koa-metrics/dist"],
+    "limit": "4.9KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-tracking-pixel/dist"],
+    "limit": "1.5KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-form-state/dist"],
+    "limit": "13.6KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/address/dist"],
+    "limit": "40.1KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-serialize/dist"],
+    "limit": "1.6KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-google-analytics/dist"],
+    "limit": "5.1KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/react-i18n/dist"],
+    "limit": "14.4KB"
+  },
+  {
+    "webpack": false,
+    "path": ["packages/i18n/dist"],
+    "limit": "2.5KB"
+  }
+]
