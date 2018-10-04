d2514e228192a195689f3efce8668a28347d48a2
Pat Sissons <pat.sissons@shopify.com>
2018-10-04 13:39:53 -0700

    adding augmentation to simplify project schemaPath resolution

--- packages/graphql-tool-utilities/augmentations.ts
+++ packages/graphql-tool-utilities/augmentations.ts
@@ -3,6 +3,7 @@ import {GraphQLProjectConfig} from 'graphql-config/lib/GraphQLProjectConfig';
 declare module 'graphql-config/lib/GraphQLProjectConfig' {
   interface GraphQLProjectConfig {
     resolveProjectName(defaultName?: string): string;
+    resolveSchemaPath(): string;
   }
 }
 
@@ -13,4 +14,21 @@ function resolveProjectName(
   return this.projectName || defaultName;
 }
 
+function resolveSchemaPath(this: GraphQLProjectConfig) {
+  // schemaPath is nullable in graphq-config even though it cannot actually be
+  // omitted. This function simplifies access ot the schemaPath without
+  // requiring a type guard.
+  if (!this.schemaPath) {
+    // this case should never happen with a properly formatted config file.
+    // graphql-config currently does not perform any validation so it's possible
+    // for a mal-formed schema to be loaded at runtime.
+    throw new Error(
+      `Missing GraphQL schemaPath for project '${this.resolveProjectName()}'`,
+    );
+  }
+
+  return this.schemaPath;
+}
+
 GraphQLProjectConfig.prototype.resolveProjectName = resolveProjectName;
+GraphQLProjectConfig.prototype.resolveSchemaPath = resolveSchemaPath;
