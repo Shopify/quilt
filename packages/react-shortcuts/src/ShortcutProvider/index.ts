a755154022a7f5bb66e828e0dc30190d4e8c0159
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-10-23 20:05:24 -0700

    react-shortcuts: export Provider for enabling external ShortcutProvider implementors
    The default `ShortcutProvider` is great for most React applications, but when
    React is being rendered into multiple locations on a page (for example, in a
    browser extension that extends multiple different locations on a page with
    React), the default `ShortcutProvider` does not work as it is intended to be a
    singleton.
    
    In order to implement one's own `ShortcutProvider` for this use case, the `ContextProvider`
    must be exported.

--- packages/react-shortcuts/src/ShortcutProvider/index.ts
+++ packages/react-shortcuts/src/ShortcutProvider/index.ts
@@ -1,4 +1,4 @@
 import ShortcutProvider from './ShortcutProvider';
 
-export {Props, Context, Consumer} from './ShortcutProvider';
+export {Props, Context, Provider, Consumer} from './ShortcutProvider';
 export default ShortcutProvider;
