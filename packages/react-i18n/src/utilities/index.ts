3cc0943a4887db750b7ff2bc5151b4e65c149d93
Loic De Laubier <loic.delaubier@shopify.com>
2018-11-01 11:58:10 -0400

    Extract utilities into multiple files. Add support for getCurrencySymbol Mock getCurrencySymbol utility in I18n test Add entry in README for getCurrencySymbol Use MissingCurrencyCodeError

---
+++ packages/react-i18n/src/utilities/index.ts
@@ -0,0 +1,15 @@
+import {getCurrencySymbol} from './money';
+import {noop} from './noop';
+import {
+  PSEUDOTRANSLATE_OPTIONS,
+  TranslateOptions,
+  translate,
+} from './translate';
+
+export {
+  getCurrencySymbol,
+  noop,
+  PSEUDOTRANSLATE_OPTIONS,
+  translate,
+  TranslateOptions,
+};
