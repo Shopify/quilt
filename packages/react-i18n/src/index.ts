fd145380f6fbaf013267aaab3513115ae7bce420
Pat Sissons <pat.sissons@shopify.com>
2018-11-02 10:14:23 -0400

    add DateStyle date formatting helpers to react-i18n

--- packages/react-i18n/src/index.ts
+++ packages/react-i18n/src/index.ts
@@ -5,4 +5,4 @@ export {default as getTranslationsFromTree} from './server';
 export {withI18n, WithI18nProps} from './decorator';
 export {translate} from './utilities';
 export {I18nDetails} from './types';
-export {Weekdays} from './constants';
+export {DateStyle, Weekdays} from './constants';
