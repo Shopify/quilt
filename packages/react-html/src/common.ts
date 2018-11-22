e2f6a13623655bf456ef08e1c11f1509ba9eb80c
Chris Sauve <chrismsauve@gmail.com>
2018-11-22 09:25:26 -0500

    Fix: export createSerializer

--- packages/react-html/src/common.ts
+++ packages/react-html/src/common.ts
@@ -2,3 +2,4 @@ export * from './components';
 export {default as Manager} from './manager';
 export {Provider} from './context';
 export {showPage, getSerialized} from './utilities';
+export {createSerializer} from './serializer';
