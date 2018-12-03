e94a3d8516c14ec6c588c78f340b6f161c6953b7
Chris Sauve <chrismsauve@gmail.com>
2018-12-02 20:12:38 -0500

    Same effect ID for title

--- packages/react-html/src/components/Title.tsx
+++ packages/react-html/src/components/Title.tsx
@@ -2,12 +2,11 @@ import * as React from 'react';
 import {Effect} from '@shopify/react-effect';
 import {Consumer} from '../context';
 import Manager from '../manager';
+import {EFFECT_ID} from '../utilities';
 
 interface Props {
   children: string;
 }
-
-export const EXTRACT_ID = Symbol('html');
 
 export default class Title extends React.PureComponent<Props> {
   private removeTitle?: ReturnType<InstanceType<typeof Manager>['addTitle']>;
@@ -26,7 +25,7 @@ export default class Title extends React.PureComponent<Props> {
         {manager => (
           <Effect
             key={title}
-            kind={EXTRACT_ID}
+            kind={EFFECT_ID}
             perform={() => {
               if (this.removeTitle) {
                 this.removeTitle();
