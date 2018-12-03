15723de41a8da6efd9394749c36e068e3dcef5f0
Nick Small <i@nickjs.com>
2018-12-03 18:36:42 -0500

    [polyfills] move module mapping into quilt
    Rename sewing-kit-config -> config

---
+++ packages/polyfills/src/config.ts
@@ -0,0 +1,50 @@
+import {isSupported} from 'caniuse-api';
+
+export interface PolyfillDescriptor {
+  supportsNode: boolean;
+  featureTest?: string;
+}
+
+export const polyfills: {[polyfill: string]: PolyfillDescriptor} = {
+  baseline: {
+    supportsNode: true,
+  },
+  fetch: {
+    supportsNode: true,
+    featureTest: 'fetch',
+  },
+  intl: {
+    supportsNode: false,
+    featureTest: 'internationalization-plural-rul',
+  },
+  url: {
+    supportsNode: true,
+    featureTest: 'urlsearchparams',
+  },
+};
+
+export function mappedPolyfillsForEnv(browser: 'node' | string | string[]) {
+  const mappedPolyfills = {};
+
+  Object.entries(polyfills).forEach(
+    ([polyfill, {supportsNode, featureTest}]) => {
+      const mapFrom = `@shopify/polyfills/${polyfill}$`;
+      const mapToPrefix = `shopify-polyfills-beta/dist/polyfills`;
+
+      if (browser === 'node') {
+        mappedPolyfills[mapFrom] = supportsNode
+          ? `${mapToPrefix}/${polyfill}.node`
+          : '';
+      } else if (featureTest) {
+        mappedPolyfills[mapFrom] = isSupported(featureTest, browser)
+          ? ''
+          : `${mapToPrefix}/${polyfill}`;
+      } else {
+        // If there's no feature we can test for, we assume we always need to polyfill it.
+        mappedPolyfills[mapFrom] = `${mapToPrefix}/${polyfill}`;
+      }
+    },
+  );
+
+  return mappedPolyfills;
+}
