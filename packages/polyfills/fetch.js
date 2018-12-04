2c233d22962dfa9e1e08222d5cc909bb0e755410
Nick Small <i@nickjs.com>
2018-12-03 23:56:34 -0500

    [polyfills] build

--- packages/polyfills/fetch.js
+++ packages/polyfills/fetch.js
@@ -1,4 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("whatwg-fetch");
-console.log('BROWSER FETCH');
--- packages/polyfills/fetch.node.js
+++ packages/polyfills/fetch.node.js
@@ -1,4 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("isomorphic-fetch");
-console.log('SERVER FETCH');
