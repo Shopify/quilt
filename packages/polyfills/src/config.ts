5877ed737a1318d063d2d3d59659314f5a1b2295
Nick Small <i@nickjs.com>
2018-12-03 23:46:57 -0500

    [polyfills] use noop import

--- packages/polyfills/src/config.ts
+++ packages/polyfills/src/config.ts
@@ -30,14 +30,15 @@ export function mappedPolyfillsForEnv(browser: 'node' | string | string[]) {
     ([polyfill, {supportsNode, featureTest}]) => {
       const mapFrom = `@shopify/polyfills/${polyfill}$`;
       const mapToPrefix = `shopify-polyfills-beta`;
+      const noop = `shopify-polyfills-beta/noop`;
 
       if (browser === 'node') {
         mappedPolyfills[mapFrom] = supportsNode
           ? `${mapToPrefix}/${polyfill}.node`
-          : '';
+          : noop;
       } else if (featureTest) {
         mappedPolyfills[mapFrom] = isSupported(featureTest, browser)
-          ? ''
+          ? noop
           : `${mapToPrefix}/${polyfill}`;
       } else {
         // If there's no feature we can test for, we assume we always need to polyfill it.
