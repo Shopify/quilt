cd55cad464e7394303059531ad582c18c2b93318
Robin Drexler <drexler.robin@gmail.com>
2019-01-18 19:29:47 +0100

    feat: allow to specify multiple env vars in withEnv

--- packages/with-env/src/test/index.test.ts
+++ packages/with-env/src/test/index.test.ts
@@ -1,16 +1,16 @@
 import withEnv from '../index';
 
-function getEnv() {
-  return process.env.NODE_ENV;
-}
-
 function wait(duration: number) {
   return new Promise(resolve => {
     setTimeout(resolve, duration);
   });
 }
 
-describe('withEnv', () => {
+describe('withEnv NODE_ENV', () => {
+  function getEnv() {
+    return process.env.NODE_ENV;
+  }
+
   it('changes the NODE_ENV for a standard Function', () => {
     withEnv('development', () => {
       expect(getEnv()).toBe('development');
@@ -30,5 +30,48 @@ describe('withEnv', () => {
       await wait(50);
       expect(getEnv()).toBe('production');
     });
+  });
+});
+
+describe('withEnv env object', () => {
+  afterEach(() => {
+    delete process.env.TEST_ONE;
+    delete process.env.TEST_TWO;
+    delete process.env.TEST_EXISTING;
+  });
+
+  it('changes multiple env vars', () => {
+    withEnv({TEST_ONE: 'foo', TEST_TWO: 'bar'}, () => {
+      expect(process.env.TEST_ONE).toBe('foo');
+      expect(process.env.TEST_TWO).toBe('bar');
+    });
+  });
+
+  it('does not override other env vars', () => {
+    process.env.TEST_EXISTING = 'existing';
+
+    withEnv({TEST_ONE: 'foo'}, () => {
+      expect(process.env.TEST_ONE).toBe('foo');
+      expect(process.env.TEST_EXISTING).toBe('existing');
+    });
+    expect(process.env.TEST_EXISTING).toBe('existing');
+  });
+
+  it('resets multiple env vars for a standard Function', () => {
+    process.env.TEST_ONE = 'pre-test';
+
+    withEnv({TEST_ONE: 'foo', TEST_TWO: 'bar'}, () => {
+      expect(process.env.TEST_ONE).toBe('foo');
+    });
+
+    expect(process.env.TEST_ONE).toBe('pre-test');
+  });
+
+  it('clears env vars that were previously undefined', () => {
+    withEnv({NON_EXISTING: 'foo'}, () => {
+      expect(process.env.NON_EXISTING).toBe('foo');
+    });
+
+    expect(process.env.NON_EXISTING).toBeUndefined();
   });
 });
