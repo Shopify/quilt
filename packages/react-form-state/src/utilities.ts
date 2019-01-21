300389252b8d367b66b93690c4cda502cb9e7025
Tzvi Melamed <tzvi@melamed.biz>
2019-01-21 12:24:02 -0500

    :fire: Remove lodash in react-form-state

--- packages/react-form-state/src/utilities.ts
+++ packages/react-form-state/src/utilities.ts
@@ -2,12 +2,10 @@ export function mapObject<Input, Output>(
   input: Input,
   mapper: (value: any, key: keyof Input) => any,
 ) {
-  return Object.keys(input)
-    .map(key => [key, input[key]])
-    .reduce((accumulator: any, [key, value]) => {
-      accumulator[key] = mapper(value, key as keyof Input);
-      return accumulator;
-    }, {}) as Output;
+  return Object.entries(input).reduce((accumulator: any, [key, value]) => {
+    accumulator[key] = mapper(value, key as keyof Input);
+    return accumulator;
+  }, {}) as Output;
 }
 
 export function push<T>(array: T[], ...values: T[]) {
@@ -26,4 +24,65 @@ export function replace<T>(array: T[], targetIndex: number, newValue: T) {
 
     return newValue;
   });
+}
+
+export function set<InputType extends Object>(
+  lhs: InputType,
+  path: string[],
+  value: any,
+) {
+  if (path.length === 0) {
+    return lhs;
+  } else if (path.length === 1) {
+    return {
+      ...(lhs as any),
+      [path[0]]: value,
+    };
+  } else {
+    const [current, ...rest] = path;
+    return {
+      ...(lhs as any),
+      [current]: set(lhs[current], rest, value),
+    } as InputType;
+  }
+}
+
+export function isEqual(value, baseline) {
+  if (typeof value !== typeof baseline) {
+    return false;
+  }
+  if (Array.isArray(value)) {
+    if (!Array.isArray(baseline)) {
+      return false;
+    }
+    if (value.length !== baseline.length) {
+      return false;
+    }
+    for (let iter = 0; iter < value.length; iter++) {
+      if (!isEqual(value[iter], baseline[iter])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  if (typeof value === 'object') {
+    if (value === null) {
+      return baseline === null;
+    }
+    if (baseline === null) {
+      return false;
+    }
+    const keysInValue = Object.keys(value);
+    const keysInBaseline = Object.keys(baseline);
+    if (keysInValue.length !== keysInBaseline.length) {
+      return false;
+    }
+    for (const key of keysInValue) {
+      if (!isEqual(value[key], baseline[key])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return value === baseline;
 }
