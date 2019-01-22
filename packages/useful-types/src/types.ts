1a42a8fcfbd9bfde61cc0683c623b3d52961dd03
Alex Salisbury <asalisbury@users.noreply.github.com>
2019-01-22 15:53:49 -0500

    Add ArrayElement type (#470)

--- packages/useful-types/src/types.ts
+++ packages/useful-types/src/types.ts
@@ -2,6 +2,7 @@ import {ComponentType} from 'react';
 
 export type ThenType<T> = T extends Promise<infer U> ? U : T;
 export type FirstArgument<T> = T extends ((arg: infer U) => any) ? U : never;
+export type ArrayElement<T> = T extends (infer U)[] ? U : never;
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 export type Props<T> = T extends ComponentType<infer P> ? P : never;
 export type DeepPartial<T> = {
