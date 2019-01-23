2ee3d5a2a4bd37134c7f1ab5c2dc2ff26dec54aa
Chris Sauve <chrismsauve@gmail.com>
2019-01-23 17:45:30 -0500

    Also update import-remote

--- packages/react-import-remote/src/ImportRemote.tsx
+++ packages/react-import-remote/src/ImportRemote.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Preconnect from '@shopify/react-preconnect';
+import {Preconnect} from '@shopify/react-html';
 import load from './load';
 
 export interface Props<Imported = any> {
@@ -29,7 +29,7 @@ export default class ImportRemote extends React.PureComponent<Props, never> {
 
     if (preconnect) {
       const url = new URL(source);
-      return <Preconnect hosts={[url.hostname]} />;
+      return <Preconnect source={url.origin} />;
     }
 
     return null;
