858d8e6fa430919304662759948182a92e8e749a
Tzvi Melamed <tzvi@melamed.biz>
2019-02-07 13:38:41 -0500

    :white_check_mark: Adds tests

--- packages/babel-plugin-react-i18n/src/test/BabelPluginReactI18n.test.ts
+++ packages/babel-plugin-react-i18n/src/test/BabelPluginReactI18n.test.ts
@@ -1,5 +1,87 @@
-describe('babel-plugin-react-i18n', () => {
-  it('works', () => {
-    // write tests for your package here!
+import * as path from 'path';
+import {transformFile} from '@babel/core';
+import BabelPluginReactI18n from '../BabelPluginReactI18n';
+
+function runFixture(fixturePath: string) {
+  return new Promise<string>((resolve, reject) => {
+    transformFile(
+      fixturePath,
+      {
+        plugins: [BabelPluginReactI18n],
+        sourceRoot: __dirname,
+        filename: fixturePath,
+        filenameRelative: path.relative(__dirname, fixturePath),
+      },
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          // eslint-disable-next-line typescript/no-non-null-assertion
+          resolve(result!.code!);
+        }
+      },
+    );
+  });
+}
+
+describe('babel-pluin-react-i18n', () => {
+  it('injects arguments when adjacent translations exist', async () => {
+    const fixturePath = path.resolve(
+      __dirname,
+      'fixtures',
+      'adjacentTranslations',
+      'MyComponent.js',
+    );
+
+    const code = await runFixture(fixturePath);
+    expect(code).toMatchInlineSnapshot(`
+"import React from 'react';
+import { withI18n } from '@shopify/react-i18n';
+import enTranslations from './translations/en.json';
+
+function MyComponent({
+  i18n
+}) {
+  return i18n.translate('key');
+}
+
+export default withI18n({
+  id: 'MyComponent_1qre7',
+  fallback: enTranslations,
+
+  async translations(locale) {
+    try {
+      const dictionary = await import(
+      /* webpackChunkName: 'MyComponent_1qre7-i18n' */
+      \`./translations/\${locale}.json\`);
+      return dictionary;
+    } catch (err) {}
+  }
+
+})(MyComponent);"
+`);
+  });
+
+  it('does not inject arguments when no adjacent translations exist', async () => {
+    const fixturePath = path.resolve(
+      __dirname,
+      'fixtures',
+      'withoutTranslations',
+      'MyComponent.js',
+    );
+
+    const code = await runFixture(fixturePath);
+    expect(code).toMatchInlineSnapshot(`
+"import React from 'react';
+import { withI18n } from '@shopify/react-i18n';
+
+function MyComponent({
+  i18n
+}) {
+  return i18n.translate('key');
+}
+
+export default withI18n()(MyComponent);"
+`);
   });
 });
