a5b394d3d773db34751e318562c38aa2b602d1e6
Tzvi Melamed <tzvi@melamed.biz>
2019-02-21 10:25:47 -0500

    Export CurrencyCode and LanguageDirection from types

--- packages/react-i18n/src/index.ts
+++ packages/react-i18n/src/index.ts
@@ -3,5 +3,5 @@ export {default as Manager, ExtractedTranslations} from './manager';
 export {default as I18n} from './i18n';
 export {withI18n, WithI18nProps} from './decorator';
 export {translate} from './utilities';
-export {I18nDetails} from './types';
+export {I18nDetails, LanguageDirection, CurrencyCode} from './types';
 export {DateStyle, Weekdays} from './constants';
