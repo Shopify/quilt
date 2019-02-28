88c146771d3046eddf43d59129172fa266c62394
Gord Pearson <gord.pearson@shopify.com>
2019-02-28 15:06:54 -0500

    ✨ `performance` now tracks first-input-delay
    Basically, this measures the milliseconds between the user's first event, and the browser passing the event on to handlers.  See https://github.com/GoogleChromeLabs/first-input-delay for more information.
    
    Note: this is only useful when the first-input-delay polyfill is present in the document's head.

--- packages/performance/src/test/performance.test.ts
+++ packages/performance/src/test/performance.test.ts
@@ -1,5 +1,9 @@
+import {FirstArgument} from '@shopify/useful-types';
 import {Performance} from '../performance';
 import {Navigation} from '../navigation';
+import {EventType} from '../types';
+
+type FirstInputDelayCallback = FirstArgument<PerfMetrics['onFirstInputDelay']>;
 
 describe('Performance', () => {
   // We are not adding all the required tests here until we have time
@@ -11,5 +15,51 @@ describe('Performance', () => {
 
     performance.finish();
     expect(spy).toHaveBeenCalledWith(expect.any(Navigation));
+  });
+
+  // This can unskip once mocks are available for either:
+  // - `performance.timing`
+  // - `PerformanceNavigationTiming` and `PerformanceObserver`
+  describe.skip('first-input-delay', () => {
+    class MockPerfMetrics implements PerfMetrics {
+      private _callback: FirstInputDelayCallback | null = null;
+
+      get callback() {
+        return this._callback;
+      }
+
+      onFirstInputDelay(callback) {
+        this._callback = callback;
+      }
+    }
+
+    it('registers a first-input-delay listener', () => {
+      const perfMetrics = new MockPerfMetrics();
+      window.perfMetrics = perfMetrics;
+
+      // eslint-disable-next-line no-new
+      new Performance();
+      expect(perfMetrics.callback).not.toBeNull();
+    });
+
+    it('sends first-interaction-delay lifecycle event', () => {
+      const perfMetrics = new MockPerfMetrics();
+      window.perfMetrics = perfMetrics;
+
+      const lifecycleSpy = jest.fn();
+      // eslint-disable-next-line no-new
+      const performance = new Performance();
+      performance.on('lifecycleEvent', lifecycleSpy);
+
+      // eslint-disable-next-line typescript/no-non-null-assertion
+      perfMetrics.callback!(1000, new Event('click'));
+
+      expect(lifecycleSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: EventType.FirstInputDelay,
+          duration: 1000,
+        }),
+      );
+    });
   });
 });
