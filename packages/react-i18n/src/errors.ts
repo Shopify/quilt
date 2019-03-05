c814f2a8c06ca00216f40208146c0f6a86e3a4ad
Chris Sauvé <chris.sauve@shopify.com>
2019-03-05 16:00:21 -0500

    Allow custom error handling (#550)

--- packages/react-i18n/src/errors.ts
+++ packages/react-i18n/src/errors.ts
@@ -3,3 +3,10 @@ export class MissingReplacementError extends Error {}
 export class MissingCurrencyCodeError extends Error {}
 export class MissingCountryError extends Error {}
 export class InvalidI18nConnectionError extends Error {}
+
+export type I18nError =
+  | MissingTranslationError
+  | MissingReplacementError
+  | MissingCurrencyCodeError
+  | MissingCountryError
+  | InvalidI18nConnectionError;
