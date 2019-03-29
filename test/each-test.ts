33acc88c5f5a18c77a8297e0ac2395770c5b44bf
Chris Sauve <chrismsauve@gmail.com>
2019-03-29 14:43:04 -0400

    Update test

--- test/each-test.ts
+++ test/each-test.ts
@@ -1,6 +1,14 @@
-import './matchers';
 import addClosest from 'element-closest';
+
+import './matchers';
+import '../packages/react-testing/src/matchers';
+
+import {destroyAll} from '../packages/react-testing/src/api';
 
 if (typeof window !== 'undefined') {
   addClosest(window);
 }
+
+afterEach(() => {
+  destroyAll();
+});
