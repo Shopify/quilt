c0bf095d2e408fa0a81581152a0deb2b31df5853
Michelle Chen <michelle.chen@shopify.com>
2019-04-09 15:49:06 -0400

    ✨ add `useQuery` hook

--- packages/react-graphql/CHANGELOG.md
+++ packages/react-graphql/CHANGELOG.md
@@ -7,6 +7,8 @@ and adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
 
+- Added `useQuery` hook ([#663](https://github.com/Shopify/quilt/pull/663))
+
 ## [3.1.0] - 2019-04-17
 
 ### Added
--- packages/react-graphql/src/hooks/index.ts
+++ packages/react-graphql/src/hooks/index.ts
@@ -1,4 +1,4 @@
-export {ApolloProvider} from './ApolloProvider';
-export {useApolloClient} from './apollo-client';
+export {default as useApolloClient} from './apollo-client';
+export {default as useQuery} from './query';
 export {default as useMutation} from './mutation';
 export * from './types';
