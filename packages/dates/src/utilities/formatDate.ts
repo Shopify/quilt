a9ed0a6f5565e9e4e7894c7aa8432d7b72deb9da
jimchou <jim.chou@shopify.com>
2019-04-09 20:31:39 -0400

    Memoize DateTimeFormat to speed up format date

--- packages/dates/src/utilities/formatDate.ts
+++ packages/dates/src/utilities/formatDate.ts
@@ -1,16 +1,38 @@
+import memoize from 'lodash/memoize';
+
+const memoizedGetDateTimeFormat = memoize(
+  getDateTimeFormat,
+  dateTimeFormatCacheKey,
+);
+
 export function formatDate(
   date: Date,
-  locales: string,
+  locales: string | string[],
   options: Intl.DateTimeFormatOptions = {},
-): string {
+) {
   // Etc/GMT+12 is not supported in most browsers and there is no equivalent fallback
   if (options.timeZone != null && options.timeZone === 'Etc/GMT+12') {
     const adjustedDate = new Date(date.valueOf() - 12 * 60 * 60 * 1000);
 
-    return Intl.DateTimeFormat(locales, {...options, timeZone: 'UTC'}).format(
-      adjustedDate,
-    );
+    return memoizedGetDateTimeFormat(locales, {
+      ...options,
+      timeZone: 'UTC',
+    }).format(adjustedDate);
   }
 
-  return Intl.DateTimeFormat(locales, options).format(date);
+  return memoizedGetDateTimeFormat(locales, options).format(date);
+}
+
+function getDateTimeFormat(
+  locales?: string | string[],
+  options?: Intl.DateTimeFormatOptions,
+) {
+  return Intl.DateTimeFormat(locales, options);
+}
+
+function dateTimeFormatCacheKey(
+  locales: string | string[],
+  options?: Intl.DateTimeFormatOptions,
+) {
+  return `${locales}-${JSON.stringify(options)}`;
 }
