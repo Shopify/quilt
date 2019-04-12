c82870ff5be70f8449610ef3593b873c780ee9ff
cartogram <matt.seccafien@shopify.com>
2019-04-12 15:23:10 -0400

    using kebob case

--- packages/react-hooks/src/hooks/index.ts
+++ packages/react-hooks/src/hooks/index.ts
@@ -1,2 +1,2 @@
-export {default as useTimeout} from './useTimeout';
-export {default as useOnValueChange} from './useOnValueChange';
+export {default as useTimeout} from './timeout';
+export {default as useOnValueChange} from './on-value-change';
