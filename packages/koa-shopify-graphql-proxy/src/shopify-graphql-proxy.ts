d5e75dbaef21fbd7f4c0c649c938da9aeddbb74c
Tzelon Machluf <tzelon47@gmail.com>
2019-08-05 00:58:40 +0300

    Update shopify-graphql-proxy.ts
    Delete cookie header before proxying graphql request.

--- packages/koa-shopify-graphql-proxy/src/shopify-graphql-proxy.ts
+++ packages/koa-shopify-graphql-proxy/src/shopify-graphql-proxy.ts
@@ -53,6 +53,10 @@ export default function shopifyGraphQLProxy(proxyOptions: ProxyOptions) {
         'Content-Type': 'application/json',
         'X-Shopify-Access-Token': accessToken,
       },
+      proxyReqOptDecorator(proxyReqOpts) {
+        delete proxyReqOpts.headers['cookie'];
+        return proxyReqOpts;
+      },
       proxyReqPathResolver() {
         return `${GRAPHQL_PATH_PREFIX}/${version}/graphql.json`;
       },
