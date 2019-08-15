a184644f826352b95dadd8718184734b85cc6134
Mallen <mathew.allen@shopify.com>
2019-08-15 13:12:54 -0400

    🔊 add more logging to the request flow for quilt_rails (#853)
    * 🔊 add more logging to the request flow
    
    * 🚨 fix breakages when the rails logger is not configured
    
    * 🚨 fix broken test

--- gems/quilt_rails/lib/quilt_rails/react_renderable.rb
+++ gems/quilt_rails/lib/quilt_rails/react_renderable.rb
@@ -5,8 +5,22 @@ module Quilt
     include ReverseProxy::Controller
 
     def render_react
-      host = "#{Quilt.configuration.react_server_protocol}://#{Quilt.configuration.react_server_host}"
-      reverse_proxy(host)
+      url = "#{Quilt.configuration.react_server_protocol}://#{Quilt.configuration.react_server_host}"
+      ReactRenderable.log("[ReactRenderable] proxying to React server at #{url}")
+
+      reverse_proxy(url) do |callbacks|
+        callbacks.on_response do |status_code, _response|
+          ReactRenderable.log("[ReactRenderable] #{url} returned #{status_code}")
+        end
+      end
+    end
+
+    def self.log(string)
+      if Rails.logger.nil?
+        puts string
+      else
+        Rails.logger.info(string)
+      end
     end
   end
 end
