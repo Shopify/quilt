52f008e8909bd1d3bcd9d86fbb8d3b6ed436f729
cartogram <matt.seccafien@shopify.com>
2019-08-20 22:08:46 -0400

    fix broken test

--- gems/quilt_rails/test/quilt_rails/react_renderable_test.rb
+++ gems/quilt_rails/test/quilt_rails/react_renderable_test.rb
@@ -5,7 +5,8 @@ module Quilt
 
     def test_render_react_calls_reverse_proxy_with_server_uri_and_csrf
       url = "#{Quilt.configuration.react_server_protocol}://#{Quilt.configuration.react_server_host}"
-      assert_equal(render_react, reverse_proxy(url))
+
+      assert_equal(render_react, reverse_proxy(url, headers: { 'X-CSRF-Token': form_authenticity_token }))
     end
 
     def reverse_proxy(url, headers: {})
