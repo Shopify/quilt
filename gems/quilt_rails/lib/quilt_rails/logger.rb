9796a9c1a8751ada83c957b42a28403c7d51da2b
Mathew Allen <mathew.allen@shopify.com>
2019-08-22 15:37:41 -0400

    🐛 fix breakages in production when used with ShopifySecurityBase

---
+++ gems/quilt_rails/lib/quilt_rails/logger.rb
@@ -0,0 +1,11 @@
+module Quilt
+  module Logger
+    def self.log(string)
+      if defined? Rails && Rails.logger.nil?
+        puts string
+      else
+        Rails.logger.info(string)
+      end
+    end
+  end
+end
