a3ed2b119dc3d2353d8b8aab46ee2b8d939bdb13
Ismail Syed <isyed867@gmail.com>
2019-08-26 14:48:50 -0400

    Add support for node projects

--- packages/react-server-webpack-plugin/src/test/fixtures/custom-base-path/webpack.config.js
+++ packages/react-server-webpack-plugin/src/test/fixtures/custom-base-path/webpack.config.js
@@ -1,33 +1,15 @@
 const {ReactServerPlugin} = require('../../../react-server-webpack-plugin');
+const {serverConfig, clientConfig} = require('../webpack-utilities');
 
 const universal = {
-  mode: 'production',
-  optimization: {
-    minimize: false,
-  },
   plugins: [new ReactServerPlugin({basePath: './app/ui'})],
 };
 
-const server = {
-  ...universal,
-  name: 'server',
-  target: 'node',
-  entry: './app/ui/server',
-  externals: [
-    (context, request, callback) => {
-      if (/node_modules/.test(context)) {
-        return callback(null, `commonjs ${request}`);
-      }
-      callback();
-    },
-  ],
-};
-
-const client = {
-  ...universal,
-  name: 'client',
-  target: 'web',
-  entry: './app/ui/client',
-};
-
-module.exports = [server, client];
+module.exports = [
+  serverConfig(universal, {
+    entry: './app/ui/server',
+  }),
+  clientConfig(universal, {
+    entry: './app/ui/client',
+  }),
+];
