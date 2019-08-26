37043f52a5920d0bba74a26ee14136f4e60d5b1a
cartogram <matt.seccafien@shopify.com>
2019-08-26 12:30:54 -0400

    Adds `stream` to `react-html/server`

---
+++ packages/react-html/src/server/tests/stream.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import stream from '../stream';
+
+describe('stream', () => {
+  it('adds a doctype to the response', async () => {
+    const app = <div>Hello stream</div>;
+
+    const result = await streamResponse(stream(app));
+
+    expect(result).toMatch(/<!DOCTYPE html>/);
+    expect(result).toMatch(/Hello stream/);
+  });
+});
+
+function streamResponse(stream: NodeJS.ReadableStream) {
+  return new Promise<string>(resolve => {
+    let response: string;
+
+    stream.on('data', data => (response += data));
+    stream.on('end', () => resolve(response));
+  });
+}
