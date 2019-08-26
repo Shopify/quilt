719e72a7afe5e847aa47996e4512f1fef90765ad
Ismail Syed <isyed867@gmail.com>
2019-08-26 14:48:50 -0400

    Add support for node projects

--- packages/react-server-webpack-plugin/src/test/fixtures/no-entrypoints/node.webpack.config.js
+++ packages/react-server-webpack-plugin/src/test/fixtures/no-entrypoints/node.webpack.config.js
@@ -5,9 +5,7 @@ const {ReactServerPlugin} = require('../../../react-server-webpack-plugin');
 const universal = {
   plugins: [
     new ReactServerPlugin({
-      port: 3000,
-      host: '127.0.0.1',
-      assetPrefix: 'https://localhost/webpack/assets',
+      rails: false,
     }),
   ],
 };
--- packages/react-server-webpack-plugin/src/test/fixtures/server-entrypoint/webpack.config.js
+++ packages/react-server-webpack-plugin/src/test/fixtures/server-entrypoint/webpack.config.js
@@ -1,33 +1,3 @@
-const {ReactServerPlugin} = require('../../../react-server-webpack-plugin');
+const {serverConfig, clientConfig} = require('../webpack-utilities');
 
-const universal = {
-  mode: 'production',
-  optimization: {
-    minimize: false,
-  },
-  plugins: [new ReactServerPlugin()],
-};
-
-const server = {
-  ...universal,
-  name: 'server',
-  target: 'node',
-  entry: './server',
-  externals: [
-    (context, request, callback) => {
-      if (/node_modules/.test(context)) {
-        return callback(null, `commonjs ${request}`);
-      }
-      callback();
-    },
-  ],
-};
-
-const client = {
-  ...universal,
-  name: 'client',
-  target: 'web',
-  entry: './client',
-};
-
-module.exports = [server, client];
+module.exports = [serverConfig(), clientConfig()];
