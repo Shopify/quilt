45edac5b7050b8679b19b143ce83bc4090502f71
cartogram <matt.seccafien@shopify.com>
2019-08-26 12:49:44 -0400

    downgrade multistream

--- packages/react-html/src/server/stream.ts
+++ packages/react-html/src/server/stream.ts
@@ -1,12 +1,12 @@
 import {Readable} from 'stream';
 import {ReactElement} from 'react';
 import {renderToStaticNodeStream} from 'react-dom/server';
-import Multistream from 'multistream';
+import multistream from 'multistream';
 
 export default function stream(tree: ReactElement<unknown>) {
   const doctype = new Readable();
   doctype.push('<!DOCTYPE html>');
   doctype.push(null);
 
-  return new Multistream([doctype, renderToStaticNodeStream(tree)]);
+  return multistream([doctype, renderToStaticNodeStream(tree)]);
 }
