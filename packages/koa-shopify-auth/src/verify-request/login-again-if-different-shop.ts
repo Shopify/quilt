c33e311d83f723bb1c295a967863170bdaa14078
Mallen <mathew.allen@shopify.com>
2019-08-30 16:36:29 -0400

    🐛 fix session bleeding between shops (#940)
    * 🐛 fix session bleeding between shops
    
    * 🗒️ Changelog

---
+++ packages/koa-shopify-auth/src/verify-request/login-again-if-different-shop.ts
@@ -0,0 +1,21 @@
+import {Context} from 'koa';
+import {NextFunction} from '../types';
+import {Routes} from './types';
+import {clearSession, redirectToAuth} from './utilities';
+
+export function loginAgainIfDifferentShop(routes: Routes) {
+  return async function loginAgainIfDifferentShopMiddleware(
+    ctx: Context,
+    next: NextFunction,
+  ) {
+    const {query, session} = ctx;
+
+    if (session && query.shop && session.shop !== query.shop) {
+      clearSession(ctx);
+      redirectToAuth(routes, ctx);
+      return;
+    }
+
+    await next();
+  };
+}
