101e2b8da2335a571638f608f7b25206155817bc
Michelle Chen <michelle.chen@shopify.com>
2019-09-10 12:57:08 -0400

    🔥 Delete files from `react-preconnect` & `react-self-serializers`

--- packages/react-self-serializers/I18n.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./dist/I18n');
--- packages/react-self-serializers/src/index.ts
+++
@@ -1,4 +0,0 @@
-export {AppBridge} from './AppBridge';
-export {createSelfSerializer} from './create-self-serializer';
-export {GraphQL} from './GraphQLComponent';
-export {I18n} from './I18n';
