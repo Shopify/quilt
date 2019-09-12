7be40592d5be37cad2d0ca4916072341aaa1e4be
Ismail Syed <isyed867@gmail.com>
2019-09-11 23:11:59 -0400

    Address some  PR feedback

--- packages/react-server/src/metrics/test/metric.test.tsx
+++ packages/react-server/src/metrics/test/metric.test.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import request from 'supertest';
 import getPort from 'get-port';
 
-import {metricsMiddleware} from '../metrics';
 import {createServer} from '../../server';
 import {mockMiddleware} from '../../test/utilities';
 
@@ -36,11 +35,11 @@ describe('createServer()', () => {
     server.close();
   });
 
-  it('responds with a X-React-Server-Request-Time header', async () => {
+  it('responds with a Server-Timing header', async () => {
     const response = await request(`${ip}:${port}`)
       .get('/')
       .then((resp: request.Response) => resp);
 
-    expect(response.header['x-react-server-request-time']).toBeDefined();
+    expect(response.header['server-timing']).toBeDefined();
   });
 });
