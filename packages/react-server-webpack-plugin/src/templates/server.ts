88385efce47dd612e64b6f16064a340b6bfbc692
Matt Seccafien <matt.seccafien@shopify.com>
2019-09-11 22:35:49 -0400

    Add DefaultProviders to webpack generated App component (#985)
    * Adds DefaultProviders
    
    * fix build errors
    
    * docs

--- packages/react-server-webpack-plugin/src/templates/server.ts
+++ packages/react-server-webpack-plugin/src/templates/server.ts
@@ -1,12 +1,16 @@
 import React from 'react';
-import {createServer} from '@shopify/react-server';
+import {createServer, DefaultProviders} from '@shopify/react-server';
 import App from 'index';
 
 const render = ctx =>
-  React.createElement(App, {
-    server: true,
-    location: ctx.request.url,
-  });
+  React.createElement(
+    DefaultProviders,
+    null,
+    React.createElement(App, {
+      server: true,
+      location: ctx.request.url,
+    }),
+  );
 
 const app = createServer({
   render,
