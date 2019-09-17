958ffdaff4a85dbcd4006af40b9c3994e5b5c89c
Matt Seccafien <matt.seccafien@shopify.com>
2019-09-17 11:23:45 -0400

    Example of no-extraneous-dependencies not working

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  extends: [
+    'plugin:shopify/typescript',
+    'plugin:shopify/react',
+    'plugin:shopify/jest',
+    'plugin:shopify/prettier',
+  ],
+  root: true,
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+  rules: {
+    'import/extensions': 'off',
+    'jsx-a11y/control-has-associated-label': 'off',
+    'node/no-extraneous-require': 'off',
+    'import/no-cycle': 'off',
+    'jest/require-tothrow-message': 'off',
+    'callback-return': 'off',
+    'jest/no-if': 'off',
+    'import/named': 'off',
+    'func-style': 'off',
+    'react/display-name': 'off',
+    'shopify/restrict-full-import': ['error', 'lodash'],
+    'shopify/jsx-no-hardcoded-content': 'off',
+    'shopify/jest/no-vague-titles': [
+      'error',
+      {
+        allow: ['all'],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.test.ts', '*.test.tsx'],
+      rules: {
+        rules: {'shopify/jsx-no-hardcoded-content': 'off'},
+      },
+    },
+    {
+      files: ['packages/react-server-webpack-plugin/**/*'],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: true,
+            optionalDependencies: true,
+            peerDependencies: true,
+            packageDir: ['./packages/react-server-webpack-plugin'],
+          },
+        ],
+      },
+    },
+  ],
+};
