543f43e26b5d49b93525814c4f9ce71f680a5fda
Matt Seccafien <matt.seccafien@shopify.com>
2019-09-18 17:11:39 -0400

    Initial changelog manipulation using ast-utilities

--- scripts/changelog-updater.ts
+++ scripts/changelog-updater.ts
@@ -1,6 +1,17 @@
 import {execSync} from 'child_process';
 import {resolve, join} from 'path';
+import {readFileSync, writeFileSync} from 'fs-extra';
 import glob from 'glob';
+import {
+  transform,
+  addReleaseToChangelog,
+} from '../packages/ast-utilities/markdown.js';
+
+interface PackageMetadata {
+  package: string;
+  version: string;
+  changelogPath: string;
+}
 
 const root = resolve(__dirname, '..');
 const packagesDir = join(root, 'packages');
@@ -8,18 +19,36 @@ const packagesDir = join(root, 'packages');
 const tagsOnHead = execSync('git --no-pager tag --contains e0d0d3ba', {
   encoding: 'utf8',
 }).trim();
+const taggedPackagesMetadata: PackageMetadata[] = tagsOnHead
+  .split('\n')
+  .slice(0, 5)
+  .map(line => {
+    const [, packageName, version] = line.split('@');
 
-const taggedPackagesMetadata = tagsOnHead.split('\n').map(line => {
-  const [, packageName, version] = line.split('@');
+    const folder = packageName && packageName.split('/')[1];
+    const changelogPath = glob.sync(
+      join(packagesDir, folder, 'CHANGELOG.md'),
+    )[0];
 
-  const folder = packageName && packageName.split('/')[1];
-  const changelog = glob.sync(join(packagesDir, folder, 'CHANGELOG.md'))[0];
+    return {
+      package: `@${packageName}`,
+      version,
+      changelogPath,
+    };
+  });
 
-  return {
-    package: `@${packageName}`,
-    version,
-    changelog,
-  };
+taggedPackagesMetadata.forEach(async ({version, changelogPath}) => {
+  const initial = readFileSync(changelogPath);
+  const date = new Date();
+
+  const newChangelog = await transform(
+    initial.toString(),
+    addReleaseToChangelog({
+      version,
+      date: `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`,
+      notes: '- Package dependency bump',
+    }),
+  );
+
+  writeFileSync(changelogPath, newChangelog);
 });
-
-console.log('taggedPackagesMetadata: ', taggedPackagesMetadata);
