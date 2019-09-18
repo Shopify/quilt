68f17e575df5f4faae55263f44873e91d384611a
Matt Seccafien <matt.seccafien@shopify.com>
2019-09-18 19:15:53 -0400

    this does seem better…

--- packages/react-server/src/server/test/server.test.tsx
+++ packages/react-server/src/server/test/server.test.tsx
@@ -1,9 +1,11 @@
-import {Server} from 'http';
 import React from 'react';
-import request from 'supertest';
-import getPort from 'get-port';
-import {createServer} from '../server';
-import {mockMiddleware} from '../../test/utilities';
+import {useTitle} from '@shopify/react-html';
+import {useAcceptLanguage} from '@shopify/react-network';
+import {
+  mockMiddleware,
+  stopServers,
+  mountAppWithServer,
+} from '../../test/utilities';
 
 jest.mock('@shopify/sewing-kit-koa', () => ({
   middleware: jest.fn(() => mockMiddleware),
@@ -16,37 +18,52 @@ jest.mock('@shopify/sewing-kit-koa', () => ({
 }));
 
 describe('createServer()', () => {
-  function MockApp() {
-    return <div>markup</div>;
-  }
-
-  let server: Server;
-  let port: number;
-  const ip = 'http://localhost';
-
-  beforeEach(async () => {
-    port = await getPort();
-
-    server = await createServer({
-      port,
-      ip,
-      render: () => <MockApp />,
-    });
-  });
-
-  afterEach(() => {
-    server.close();
+  afterAll(() => {
+    stopServers();
   });
 
   it('starts a server that responds with markup', async () => {
-    const response = await request(`${ip}:${port}`)
-      .get('/')
-      .then((resp: request.Response) => {
-        return resp;
-      });
+    function MockApp() {
+      return <div>markup</div>;
+    }
+
+    const response = await mountAppWithServer(<MockApp />);
 
     expect(response.text).toBe(
       `<!DOCTYPE html><html lang="en"><head><meta charSet="utf-8"/><meta http-equiv="X-UA-Compatible" content="IE=edge"/><meta name="referrer" content="never"/></head><body><div id="app"><div>markup</div></div></body></html>`,
+    );
+  });
+
+  it('supports updatable meta components', async () => {
+    const myTitle = 'Shopify Mock App';
+
+    function MockApp() {
+      useTitle(myTitle);
+      return null;
+    }
+
+    const response = await mountAppWithServer(<MockApp />);
+
+    expect(response.text).toStrictEqual(
+      expect.stringContaining(
+        `<title data-react-html="true">${myTitle}</title>`,
+      ),
+    );
+  });
+
+  it('supports setting a custom locale', async () => {
+    const language = 'it';
+
+    function MockApp() {
+      const [lang] = useAcceptLanguage({code: language, quality: 1});
+      const markup = `The accept language header is ${lang.code}`;
+      return <div>{markup}</div>;
+    }
+
+    const response = await mountAppWithServer(<MockApp />);
+
+    expect(response.text).toStrictEqual(
+      expect.stringContaining(`The accept language header is ${language}`),
     );
   });
 });
