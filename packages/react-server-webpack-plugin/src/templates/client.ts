e5ce8c5675c05499f431ed570f6b903bd71755d4
Matt Seccafien <matt.seccafien@shopify.com>
2019-09-18 13:04:38 -0400

    feedback

--- packages/react-server-webpack-plugin/src/templates/client.ts
+++ packages/react-server-webpack-plugin/src/templates/client.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {showPage} from '@shopify/react-html';
+// @ts-ignore
 import App from 'index';
 
 const appContainer = document.getElementById('app');
