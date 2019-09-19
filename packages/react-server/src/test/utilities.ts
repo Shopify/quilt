bbc1a9efd747129d93e25d491b93738311509e2a
Matt Seccafien <matt.seccafien@shopify.com>
2019-09-19 17:40:07 -0400

    Better server tests (#1014)
    * this does seem better…
    
    * remove other test, one is good for now
    
    * mat feedback
    
    * remove supertest

--- packages/react-server/src/test/utilities.ts
+++ packages/react-server/src/test/utilities.ts
@@ -1,5 +1,28 @@
+import {Server} from 'http';
 import {Context} from 'koa';
+import getPort from 'get-port';
+
 import {KoaNextFunction} from '../types';
+
+export class TestRack {
+  private servers: Server[] = [];
+
+  unmountAll() {
+    this.servers.forEach(server => server.close());
+  }
+
+  async mount(mountFunction: ({port: number, ip: string}) => Server) {
+    const port = await getPort();
+    const ip = 'http://localhost';
+    const server = mountFunction({port, ip});
+
+    this.servers.push(server);
+
+    return {
+      request: () => fetch(`${ip}:${port}`).then(response => response),
+    };
+  }
+}
 
 export async function mockMiddleware(_: Context, next: KoaNextFunction) {
   await next();
