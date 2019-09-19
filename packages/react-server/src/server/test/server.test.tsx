3fd9182c479dcb7cd19929a46e6470ef5d798bce
Matt Seccafien <matt.seccafien@shopify.com>
2019-09-19 17:40:07 -0400

    Better server tests (#1014)
    * this does seem better…
    
    * remove other test, one is good for now
    
    * mat feedback
    
    * remove supertest

--- packages/react-server/src/server/test/server.test.tsx
+++ packages/react-server/src/server/test/server.test.tsx
@@ -1,9 +1,14 @@
-import {Server} from 'http';
 import React from 'react';
-import request from 'supertest';
-import getPort from 'get-port';
+import {useTitle} from '@shopify/react-html';
 import {createServer} from '../server';
-import {mockMiddleware} from '../../test/utilities';
+import {
+  mockMiddleware,
+  stopServers,
+  mountAppWithServer,
+  TestRack,
+} from '../../test/utilities';
+
+const rack = new TestRack();
 
 jest.mock('@shopify/sewing-kit-koa', () => ({
   middleware: jest.fn(() => mockMiddleware),
@@ -16,37 +21,42 @@ jest.mock('@shopify/sewing-kit-koa', () => ({
 }));
 
 describe('createServer()', () => {
-  function MockApp() {
-    return <div>markup</div>;
-  }
-
-  let server: Server;
-  let port: number;
-  const ip = 'http://localhost';
-
-  beforeEach(async () => {
-    port = await getPort();
-
-    server = await createServer({
-      port,
-      ip,
-      render: () => <MockApp />,
-    });
-  });
-
-  afterEach(() => {
-    server.close();
+  afterAll(() => {
+    rack.unmountAll();
   });
 
   it('starts a server that responds with markup', async () => {
-    const response = await request(`${ip}:${port}`)
-      .get('/')
-      .then((resp: request.Response) => {
-        return resp;
-      });
+    function MockApp() {
+      return <div>markup</div>;
+    }
 
-    expect(response.text).toBe(
+    const wrapper = await rack.mount(({ip, port}) =>
+      createServer({port, ip, render: () => <MockApp />}),
+    );
+    const response = await wrapper.request();
+
+    expect(await response.text()).toStrictEqual(
       `<!DOCTYPE html><html lang="en"><head><meta charSet="utf-8"/><meta http-equiv="X-UA-Compatible" content="IE=edge"/><meta name="referrer" content="never"/></head><body><div id="app"><div>markup</div></div></body></html>`,
+    );
+  });
+
+  it('supports updatable meta components', async () => {
+    const myTitle = 'Shopify Mock App';
+
+    function MockApp() {
+      useTitle(myTitle);
+      return null;
+    }
+
+    const wrapper = await rack.mount(({ip, port}) =>
+      createServer({port, ip, render: () => <MockApp />}),
+    );
+    const response = await wrapper.request();
+
+    expect(await response.text()).toStrictEqual(
+      expect.stringContaining(
+        `<title data-react-html="true">${myTitle}</title>`,
+      ),
     );
   });
 });
