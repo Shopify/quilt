62b247343c0cbe9d3238b289198d9bf1711c11bd
Eric Walker <eric.ewalker@gmail.com>
2019-09-24 13:23:33 -0400

    minor tweak to introduce the smallest change

--- gems/quilt_rails/lib/generators/sewing_kit/templates/sewing-kit.config.ts
+++ gems/quilt_rails/lib/generators/sewing_kit/templates/sewing-kit.config.ts
@@ -1,10 +1,11 @@
+/* eslint-env node */
+
 import {Plugins} from '@shopify/sewing-kit';
 
 function sewingKitConfig() {
   return {
-    name: 'your-app-name'
+    name: 'your-app-name',
   };
 };
 
 export default sewingKitConfig;
-
