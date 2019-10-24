2c0f7c6b11d4f085aa7984b5ce190767f0f566ff
Sylvain Hamann <sylvain.hamann@shopify.com>
2019-10-24 15:11:22 -0400

    Add usePrevious react hook

--- packages/react-hooks/src/hooks/index.ts
+++ packages/react-hooks/src/hooks/index.ts
@@ -2,3 +2,4 @@ export {useLazyRef} from './lazy-ref';
 export {default as useTimeout} from './timeout';
 export {default as useOnValueChange} from './on-value-change';
 export {useMountedRef} from './mounted-ref';
+export {default as usePrevious} from './previous';
