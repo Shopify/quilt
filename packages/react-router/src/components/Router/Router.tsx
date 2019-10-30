3294541afa61278bfc0a0a030cec8681ff00ba47
Mallen <mathew.allen@shopify.com>
2019-10-30 15:06:15 -0400

    🐛 fail fast when there is no location prop on the server (#1159)
    * 🐛 fail fast when there is no location prop on the server
    
    * 🗒️ CHANGELOG

--- packages/react-router/src/components/Router/Router.tsx
+++ packages/react-router/src/components/Router/Router.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import {StaticRouter, BrowserRouter} from 'react-router-dom';
-import {isServer} from './utilities';
+import {isClient} from './utilities';
 
 interface Props {
   location?: string;
   children?: React.ReactNode;
 }
 
+export const NO_LOCATION_ERROR =
+  'A location must be passed to <Router /> on the server.';
+
 export default function Router({location, children}: Props) {
-  return isServer() && location ? (
+  if (isClient()) {
+    return <BrowserRouter>{children}</BrowserRouter>;
+  }
+
+  if (location == null) {
+    throw new Error(NO_LOCATION_ERROR);
+  }
+
+  return (
     <StaticRouter location={location} context={{}}>
       {children}
     </StaticRouter>
-  ) : (
-    <BrowserRouter>{children}</BrowserRouter>
   );
 }
