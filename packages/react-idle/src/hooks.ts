35a174421350bc94538a53b339f4078c32b55f15
Ismail Syed <isyed867@gmail.com>
2019-11-01 13:43:26 -0400

    Fix bad rebase

--- packages/react-idle/src/hooks.ts
+++ packages/react-idle/src/hooks.ts
@@ -1,4 +1,5 @@
 import {useEffect, useRef} from 'react';
+import {ExtendedWindow} from '@shopify/useful-types';
 
 import {
   RequestIdleCallbackHandle,
@@ -14,9 +15,9 @@ export function useIdleCallback(
 
   useEffect(() => {
     if ('requestIdleCallback' in window) {
-      handle.current = (window as WindowWithRequestIdleCallback).requestIdleCallback(
-        () => callback(),
-      );
+      handle.current = (window as ExtendedWindow<
+        WindowWithRequestIdleCallback
+      >).requestIdleCallback(() => callback());
     } else if (unsupportedBehavior === UnsupportedBehavior.AnimationFrame) {
       handle.current = window.requestAnimationFrame(() => {
         callback();
@@ -34,9 +35,9 @@ export function useIdleCallback(
       }
 
       if ('cancelIdleCallback' in window) {
-        (window as WindowWithRequestIdleCallback).cancelIdleCallback(
-          currentHandle,
-        );
+        (window as ExtendedWindow<
+          WindowWithRequestIdleCallback
+        >).cancelIdleCallback(currentHandle);
       } else if (unsupportedBehavior === UnsupportedBehavior.AnimationFrame) {
         window.cancelAnimationFrame(currentHandle);
       }
