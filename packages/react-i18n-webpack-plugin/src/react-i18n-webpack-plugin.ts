0e365f3830fc8dc8e677911c4f6ac92d6e5aedc8
Michelle Chen <michelle.chen@shopify.com>
2019-11-14 18:10:52 -0500

    ✨ add ConstDependency.Template

--- packages/react-i18n-webpack-plugin/src/react-i18n-webpack-plugin.ts
+++ packages/react-i18n-webpack-plugin/src/react-i18n-webpack-plugin.ts
@@ -1,4 +1,5 @@
 import webpack, {Parser} from 'webpack';
+import {ConstDependency} from 'webpack/lib/dependencies/ConstDependency';
 import VirtualModulesPlugin from 'webpack-virtual-modules';
 
 import {ReactI18nParserPlugin, Options} from './react-i18n-parser-plugin';
@@ -21,9 +22,14 @@ export class ReactI18nPlugin {
   apply(compiler: webpack.Compiler) {
     this.virtualModules.apply(compiler);
 
-    compiler.hooks.normalModuleFactory.tap(
+    compiler.hooks.compilation.tap(
       'ReactI18nPlugin',
-      (normalModuleFactory: webpack.compilation.NormalModuleFactory) => {
+      (compilation, {normalModuleFactory}) => {
+        compilation.dependencyTemplates.set(
+          ConstDependency,
+          new ConstDependency.Template(),
+        );
+
         const handler = (parser: Parser) => {
           new ReactI18nParserPlugin(this.options, this.virtualModules).apply(
             parser,
