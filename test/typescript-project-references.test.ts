c84ef4b788316154f06fdb420871484ba2e2b997
Ismail Syed <isyed867@gmail.com>
2019-11-13 23:33:51 -0500

    Add tests for project reference things

---
+++ test/typescript-project-references.test.ts
@@ -0,0 +1,29 @@
+import {resolve} from 'path';
+
+import {readJSONSync} from 'fs-extra';
+import glob from 'glob';
+
+const IGNORE_REGEX = [
+  /tsconfig\.json/,
+  /tsconfig_base\.json/,
+  /react-self-serializer/,
+];
+
+const ROOT = resolve(__dirname, '..');
+const projectReferencesConfig = resolve(ROOT, 'packages', 'tsconfig.json');
+
+describe('typescript project references', () => {
+  it('includes all the packages', () => {
+    const referencesConfig = readJSONSync(projectReferencesConfig);
+    const references = referencesConfig.references.map(({path}) =>
+      path.replace('./', ''),
+    );
+
+    const packages = glob
+      .sync(resolve(ROOT, 'packages', '*'))
+      .filter(filePath => !IGNORE_REGEX.some(regex => regex.test(filePath)))
+      .map(packagePath => packagePath.split('quilt/packages/')[1]);
+
+    expect(packages).toStrictEqual(references);
+  });
+});
