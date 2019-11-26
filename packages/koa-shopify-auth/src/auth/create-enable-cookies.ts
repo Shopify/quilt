ac0d79a5c0b521209d29a8b5bce9162ea3cd09c5
Bastian Kistner <bastian.kistner@gmail.com>
2019-11-26 20:40:17 +0100

    added prefix to setCookieAndRedirect

--- packages/koa-shopify-auth/src/auth/create-enable-cookies.ts
+++ packages/koa-shopify-auth/src/auth/create-enable-cookies.ts
@@ -11,7 +11,7 @@ const FOOTER = `Cookies let the app authenticate you by temporarily storing your
 information. They expire after 30 days.`;
 const ACTION = 'Enable cookies';
 
-export default function createEnableCookies({apiKey}: OAuthStartOptions) {
+export default function createEnableCookies({apiKey, prefix}: OAuthStartOptions) {
   return function enableCookies(ctx: Context) {
     const {query} = ctx;
     const {shop} = query;
@@ -371,7 +371,7 @@ export default function createEnableCookies({apiKey}: OAuthStartOptions) {
     (function() {
       function setCookieAndRedirect() {
         document.cookie = "shopify.cookies_persist=true";
-        window.location.href = "/auth?shop=${shop}"
+        window.location.href = "${prefix}/auth?shop=${shop}"
       }
 
       function shouldDisplayPrompt() {
