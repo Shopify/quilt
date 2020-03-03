2718a44ff02ac59f8360c798a797c860ffc697f7
Tay Kang Sheng <kangsheng.tay@shopify.com>
2020-03-03 17:08:09 +0800

    update wrong type signature

--- packages/address-consts/src/index.ts
+++ packages/address-consts/src/index.ts
@@ -41,12 +41,12 @@ export interface Zone {
 }
 export interface LoadCountriesResponse {
   data: {countries: Country[]};
-  errors?: [any];
+  errors?: any[];
 }
 
 export interface LoadCountryResponse {
   data: {country: Country};
-  errors?: [any];
+  errors?: any[];
 }
 export interface Country {
   name: string;
