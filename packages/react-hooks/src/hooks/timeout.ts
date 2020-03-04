b6706928dbd897f973e9bc8711b0a5ac5fa0b694
Sam Bostock <sam.bostock@shopify.com>
2020-03-04 16:19:00 -0500

    Improve hook

--- packages/react-hooks/src/hooks/timeout.ts
+++ packages/react-hooks/src/hooks/timeout.ts
@@ -1,8 +1,22 @@
-import React from 'react';
+import {useEffect, useRef} from 'react';
 
-export function useTimeout(callback: () => void, delay: number) {
-  React.useEffect(() => {
-    const id = setTimeout(callback, delay);
-    return () => clearTimeout(id);
-  }, [callback, delay]);
+type IntervalCallback = () => void;
+type IntervalDelay = number | null;
+
+export function useTimeout(callback: IntervalCallback, delay: IntervalDelay) {
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setTimeout(tick, delay);
+      return () => clearTimeout(id);
+    }
+  }, [delay]);
 }
