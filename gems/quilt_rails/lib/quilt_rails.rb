b1e811901b0e3f1cc079df6ada1d9d88c4a9f0d2
Mathew  Allen <mathew.allen@shopify.com>
2020-03-20 15:44:17 -0400

    ✨ make csrf less of with quilt_rails and graphql when needed, and enhance guidelines for when to just use null_session instead

--- gems/quilt_rails/lib/quilt_rails.rb
+++ gems/quilt_rails/lib/quilt_rails.rb
@@ -9,4 +9,5 @@ require "quilt_rails/configuration"
 require "quilt_rails/react_renderable"
 require "quilt_rails/performance"
 require "quilt_rails/trusted_ui_server_csrf_strategy"
+require "quilt_rails/header_csrf_strategy"
 require "quilt_rails/monkey_patches/active_support_reloader" if Rails.env.development?
