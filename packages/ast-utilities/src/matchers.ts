1235dadf98807e0220c3e56d738c5ee826d63b65
Victor Sumner <victor.sumner@shopify.com>
2020-04-16 17:48:01 -0400

    update matchers to match @types/jest

--- packages/ast-utilities/src/matchers.ts
+++ packages/ast-utilities/src/matchers.ts
@@ -7,7 +7,7 @@ import {
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
-    interface Matchers<R, T> {
+    interface Matchers<R, T = {}> {
       toBeFormated(expected: string): void;
     }
   }
--- packages/react-i18n/src/test/matchers.ts
+++ packages/react-i18n/src/test/matchers.ts
@@ -1,7 +1,7 @@
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
-    interface Matchers<R, T> {
+    interface Matchers<R, T = {}> {
       toBeArrayOfUniqueItems(): void;
     }
   }
