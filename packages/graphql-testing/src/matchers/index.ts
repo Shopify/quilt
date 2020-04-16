3cba7895f31a9f42c671e0690ef464560a9fca52
Victor Sumner <victor.sumner@shopify.com>
2020-04-16 17:48:01 -0400

    update matchers to match @types/jest

--- packages/graphql-testing/src/matchers/index.ts
+++ packages/graphql-testing/src/matchers/index.ts
@@ -5,7 +5,7 @@ import {toHavePerformedGraphQLOperation} from './operations';
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
-    interface Matchers<R, T> {
+    interface Matchers<R, T = {}> {
       toHavePerformedGraphQLOperation<Variables>(
         document: GraphQLOperation<any, Variables, any>,
         variables?: Partial<Variables>,
--- test/matchers/index.ts
+++ test/matchers/index.ts
@@ -3,7 +3,7 @@ import 'jest-enzyme';
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
-    interface Matchers<R, T> {
+    interface Matchers<R, T = {}> {
       toBeArrayOfUniqueItems(): void;
     }
   }
