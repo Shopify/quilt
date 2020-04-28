5c9a4e9ccf449113fcd027f70b9d65a3033cc8d6
Ayron Little <ayron.little@shopify.com>
2020-04-28 13:44:37 -0400

    hack fix

--- packages/koa-shopify-auth/src/auth/redirection-page.ts
+++ packages/koa-shopify-auth/src/auth/redirection-page.ts
@@ -1,23 +1,54 @@
 export default function redirectionScript({origin, redirectTo, apiKey}) {
   return `
+    <button id="AcceptCookies">Request storage access</button>
     <script src="https://unpkg.com/@shopify/app-bridge@^1"></script> <script type="text/javascript">
       document.addEventListener('DOMContentLoaded', function() {
         if (window.top === window.self) {
           // If the current window is the 'parent', change the URL by setting location.href
           window.location.href = '${redirectTo}';
         } else {
-          // If the current window is the 'child', change the parent's URL with postMessage
-          var AppBridge = window['app-bridge'];
-          var createApp = AppBridge.default;
-          var Redirect = AppBridge.actions.Redirect;
-          var app = createApp({
-            apiKey: '${apiKey}',
-            shopOrigin: '${origin}',
-          });
-          var redirect = Redirect.create(app);
-          redirect.dispatch(Redirect.Action.REMOTE, '${redirectTo}');
+          var promise = document.requestStorageAccess();
+          promise.then(
+            function () {
+              // If the current window is the 'child', change the parent's URL with postMessage
+              var AppBridge = window['app-bridge'];
+              var createApp = AppBridge.default;
+              var Redirect = AppBridge.actions.Redirect;
+              var app = createApp({
+                apiKey: '${apiKey}',
+                shopOrigin: '${origin}',
+              });
+              var redirect = Redirect.create(app);
+              redirect.dispatch(Redirect.Action.REMOTE, '${redirectTo}');
+            },
+            function () {
+              var button = document.querySelector('#AcceptCookies');
+              button.addEventListener('click', requesty);
+            }
+          );
         }
       });
+      function requesty () {
+        var promise = document.requestStorageAccess();
+        promise.then(
+          function () {
+            // If the current window is the 'child', change the parent's URL with postMessage
+            console.log("it was accepted");
+            var AppBridge = window['app-bridge'];
+            var createApp = AppBridge.default;
+            var Redirect = AppBridge.actions.Redirect;
+            var app = createApp({
+              apiKey: '${apiKey}',
+              shopOrigin: '${origin}',
+            });
+            var redirect = Redirect.create(app);
+            redirect.dispatch(Redirect.Action.REMOTE, '${redirectTo}');
+          },
+          function () {
+            console.log("it was rejected");
+          }
+        );
+      }
     </script>
   `;
 }
