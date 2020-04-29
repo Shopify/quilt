96d7ee4f06a83fb34d84e8c9a83d16f71da87890
Mike Ragalie <mike.ragalie@shopify.com>
2020-04-29 09:56:09 -0400

    Fixing Safari 13.1 cookie loop
    Co-authored-by: Mike Ragalie <mike.ragalie@shopify.com>
    Co-authored-by: Tim Anema <tim.anema@shopify.com>

--- packages/koa-shopify-auth/src/auth/test/enable-cookies.test.ts
+++ packages/koa-shopify-auth/src/auth/test/enable-cookies.test.ts
@@ -26,6 +26,6 @@ describe('CreateEnableCookies', () => {
     expect(ctx.body).toContain('CookiePartitionPrompt');
     expect(ctx.body).toContain(baseConfig.apiKey);
     expect(ctx.body).toContain(shopOrigin);
-    expect(ctx.body).toContain(`window.location.href = "/auth?shop=${shop}"`);
+    expect(ctx.body).toContain(`redirectUrl: "/auth?shop=${shop}"`);
   });
 });
