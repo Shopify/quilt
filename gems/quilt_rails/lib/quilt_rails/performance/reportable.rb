b757bfaf550714c3a034b1c5d173cdc5f44c74ff
Brian Ryu <brian.ryu@shopify.com>
2020-05-15 14:22:17 -0400

    Parse plaintext data into JSON
    Remove redundant key processing

--- gems/quilt_rails/lib/quilt_rails/performance/reportable.rb
+++ gems/quilt_rails/lib/quilt_rails/performance/reportable.rb
@@ -4,7 +4,9 @@ module Quilt
   module Performance
     module Reportable
       def process_report(&block)
-        params.transform_keys! { |key| key.underscore.to_sym }
+        if request.content_type == 'text/plain'
+          self.params = ActionController::Parameters.new(JSON.parse(request.body.read))
+        end
         Client.send!(Report.from_params(params), &block)
       end
     end
