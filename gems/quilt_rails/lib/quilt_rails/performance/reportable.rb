c202e1157997389658725ec09a96e654833f1537
Brian Ryu <brian.ryu@shopify.com>
2020-05-19 12:01:24 -0400

    Use more idiomatic implementation

--- gems/quilt_rails/lib/quilt_rails/performance/reportable.rb
+++ gems/quilt_rails/lib/quilt_rails/performance/reportable.rb
@@ -4,12 +4,15 @@ module Quilt
   module Performance
     module Reportable
       def process_report(&block)
-        report_params = if request.content_type == 'text/plain'
-          ActionController::Parameters.new(JSON.parse(request.body.read))
-        else
-          params
-        end
-        Client.send!(Report.from_params(report_params), &block)
+        Client.send!(Report.from_params(normalized_params), &block)
+      end
+
+      private
+
+      def normalized_params
+        return params unless request.content_type == 'text/plain'
+
+        ActionController::Parameters.new(JSON.parse(request.body.read))
       end
     end
   end
