114ce1bfc321a53f16fb67e11ecefe983d0e6d99
Seamus Abshere <seamus@abshere.net>
2020-05-23 12:17:08 -0400

    Prevent XSS attack using /auth/enable_cookies
    https://example.com/shopify/auth/enable_cookies?shop=%27%22%3C/title%3E%3C/style%3E%3C/textarea%3E%3C/noscript%3E%3C/script%3E--%3E%3Cdtfy%3E%3Cscript%3Ealert(%27hello%20world%27)%3C/script%3E
    
    Thanks to @nhusher

--- packages/koa-shopify-auth/src/auth/client/top-level-interaction.ts
+++ packages/koa-shopify-auth/src/auth/client/top-level-interaction.ts
@@ -3,7 +3,7 @@ const topLevelInteraction = (shop: string, prefix = '') => {
   return `(function() {
       function setUpTopLevelInteraction() {
         var TopLevelInteraction = new ITPHelper({
-          redirectUrl: "${prefix}/auth?shop=${shop}",
+          redirectUrl: "${prefix}/auth?shop=${encodeURIComponent(shop)}",
         });
 
         TopLevelInteraction.execute();
