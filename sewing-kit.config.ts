dea94c4d04a96e5aaadfdb232bed6aa875f04d14
Brian Ryu <brian.ryu@shopify.com>
2020-06-25 12:40:37 -0400

    Add root tsconfig.json

--- sewing-kit.config.ts
+++ sewing-kit.config.ts
@@ -1,5 +1,8 @@
 import {createWorkspace} from '@sewing-kit/config';
+import {eslint} from '@sewing-kit/plugin-eslint';
+import {jest} from '@sewing-kit/plugin-jest';
+import {workspaceTypeScript} from '@sewing-kit/plugin-typescript';
 
 export default createWorkspace(workspace => {
-  workspace.use();
+  workspace.use(eslint(), jest(), workspaceTypeScript());
 });
