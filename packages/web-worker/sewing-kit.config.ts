3680eec2a7f38b43575bd504d95faa9be3758fa3
Brian Ryu <brian.ryu@shopify.com>
2020-06-30 18:42:26 -0400

    Set up web-worker entrypoints

--- packages/web-worker/sewing-kit.config.ts
+++ packages/web-worker/sewing-kit.config.ts
@@ -4,5 +4,8 @@ import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage(pkg => {
   pkg.entry({root: './src/index'});
+  pkg.entry({name: 'babel', root: './src/babel-plugin'});
+  pkg.entry({name: 'webpack', root: './src/webpack-parts'});
+  pkg.entry({name: 'worker', root: './src/worker'});
   pkg.use(quiltPackage());
 });
--- packages/web-worker/worker.d.ts
+++
@@ -1,2 +0,0 @@
-export {default} from './dist/src/worker';
-export * from './dist/src/worker';
