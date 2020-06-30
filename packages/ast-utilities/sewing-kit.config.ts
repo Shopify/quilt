cc8744bf244361b428fdfc972ae008f7ac31d819
Brian Ryu <brian.ryu@shopify.com>
2020-06-30 17:43:05 -0400

    Set up ast-utilities entrypoints

--- packages/ast-utilities/sewing-kit.config.ts
+++ packages/ast-utilities/sewing-kit.config.ts
@@ -4,5 +4,7 @@ import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage(pkg => {
   pkg.entry({root: './src/index'});
+  pkg.entry({name: 'javascript', root: './src/javascript'});
+  pkg.entry({name: 'markdown', root: './src/markdown'});
   pkg.use(quiltPackage());
 });
