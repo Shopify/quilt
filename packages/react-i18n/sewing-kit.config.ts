ce5ec223f2b7ee903a793960e4312d7066664899
Brian Ryu <brian.ryu@shopify.com>
2020-06-30 18:28:35 -0400

    Set up react-i18n entrypoints

--- packages/react-i18n/sewing-kit.config.ts
+++ packages/react-i18n/sewing-kit.config.ts
@@ -4,5 +4,14 @@ import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage(pkg => {
   pkg.entry({root: './src/index'});
+  pkg.entry({
+    name: 'generate-dictionaries',
+    root: './src/babel-plugin/generate-dictionaries',
+  });
+  pkg.entry({
+    name: 'generate-index',
+    root: './src/babel-plugin/generate-index',
+  });
+  pkg.entry({name: 'babel', root: './src/babel-plugin'});
   pkg.use(quiltPackage());
 });
