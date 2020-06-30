670621ed101540ef5f1e701f560074a400e0aec2
Brian Ryu <brian.ryu@shopify.com>
2020-06-30 18:04:45 -0400

    Set up react-async entrypoints

--- packages/react-async/sewing-kit.config.ts
+++ packages/react-async/sewing-kit.config.ts
@@ -4,5 +4,6 @@ import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage(pkg => {
   pkg.entry({root: './src/index'});
+  pkg.entry({name: 'testing', root: './src/testing'});
   pkg.use(quiltPackage());
 });
