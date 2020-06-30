08738108f8ff8085c729480d53a459ec660c03b7
Brian Ryu <brian.ryu@shopify.com>
2020-06-30 17:53:42 -0400

    Set up graphql-persisted entrypoints

--- packages/graphql-persisted/sewing-kit.config.ts
+++ packages/graphql-persisted/sewing-kit.config.ts
@@ -3,6 +3,7 @@ import {createPackage, Runtime} from '@sewing-kit/config';
 import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage(pkg => {
-  pkg.entry({root: './src/index'});
+  pkg.entry({name: 'koa', root: './src/koa-middleware'});
+  pkg.entry({name: 'apollo', root: './src/apollo', runtime: Runtime.Node});
   pkg.use(quiltPackage());
 });
