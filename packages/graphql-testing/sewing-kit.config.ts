df3be4d171eef770530bc076e96417bc192973c3
Brian Ryu <brian.ryu@shopify.com>
2020-06-30 17:59:02 -0400

    Set up graphql-testing entrypoints

--- packages/graphql-testing/sewing-kit.config.ts
+++ packages/graphql-testing/sewing-kit.config.ts
@@ -3,6 +3,7 @@ import {createPackage, Runtime} from '@sewing-kit/config';
 import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage(pkg => {
-  pkg.entry({root: './src/index'});
+  pkg.entry({root: './src/index', runtime: Runtime.Node});
+  pkg.entry({name: 'matchers', root: './src/matchers', runtime: Runtime.Node});
   pkg.use(quiltPackage());
 });
