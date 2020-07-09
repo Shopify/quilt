29c9333c708b0e14dca7c2a54058a4be2ecb11fb
Brian Ryu <brian.ryu@shopify.com>
2020-07-09 12:51:17 -0400

    Get most of the tests working

--- config/sewing-kit/index.ts
+++ config/sewing-kit/index.ts
@@ -1,10 +1,14 @@
-import {Package, createComposedProjectPlugin} from '@sewing-kit/plugins';
+import {
+  Package,
+  createComposedProjectPlugin,
+  createProjectTestPlugin,
+} from '@sewing-kit/plugins';
 import {react} from '@sewing-kit/plugin-react';
 import {javascript} from '@sewing-kit/plugin-javascript';
 import {typescript} from '@sewing-kit/plugin-typescript';
 import {buildFlexibleOutputs} from '@sewing-kit/plugin-package-flexible-outputs';
 
-export function quiltPackage({binaryOnly = false} = {}) {
+export function quiltPackage({binaryOnly = false, jestEnv = 'jsdom'} = {}) {
   return createComposedProjectPlugin<Package>('Quilt.DefaultProject', [
     javascript(),
     typescript(),
@@ -12,6 +16,31 @@ export function quiltPackage({binaryOnly = false} = {}) {
     buildFlexibleOutputs({
       esnext: !binaryOnly,
       esmodules: !binaryOnly,
+    }),
+    createProjectTestPlugin('Quilt.JestEnvironment', ({hooks}) => {
+      hooks.configure.hook(hooks => {
+        hooks.jestEnvironment?.hook(_ => jestEnv);
+      });
+    }),
+    createProjectTestPlugin('Quilt.JestConfig', ({hooks}) => {
+      hooks.configure.hook(hooks => {
+        hooks.jestConfig?.hook(config => ({
+          ...config,
+          setupFiles: ['../../test/setup.ts'],
+          setupFilesAfterEnv: ['../../test/each-test.ts'],
+        }));
+      });
+    }),
+    createProjectTestPlugin('Quilt.BabelTest', ({hooks}) => {
+      hooks.configure.hook(hooks => {
+        hooks.babelConfig?.hook(_ => ({
+          presets: [
+            ['babel-preset-shopify/node', {typescript: true}],
+            'babel-preset-shopify/react',
+          ],
+          sourceMaps: 'inline',
+        }));
+      });
     }),
   ]);
 }
--- sewing-kit.config.ts
+++ sewing-kit.config.ts
@@ -1,8 +1,24 @@
 import {createWorkspace} from '@sewing-kit/config';
+import {createWorkspaceBuildPlugin} from '@sewing-kit/plugins';
 import {eslint} from '@sewing-kit/plugin-eslint';
 import {jest} from '@sewing-kit/plugin-jest';
 import {workspaceTypeScript} from '@sewing-kit/plugin-typescript';
 
 export default createWorkspace(workspace => {
-  workspace.use(eslint(), jest(), workspaceTypeScript());
+  workspace.use(
+    eslint(),
+    jest(),
+    workspaceTypeScript(),
+    createWorkspaceBuildPlugin('Quilt.BabelBuild', ({hooks}) => {
+      hooks.configure.hook(hooks => {
+        hooks.babelConfig?.hook(_ => ({
+          presets: [
+            ['babel-preset-shopify/node', {typescript: true}],
+            'babel-preset-shopify/react',
+          ],
+          sourceMaps: 'inline',
+        }));
+      });
+    }),
+  );
 });
