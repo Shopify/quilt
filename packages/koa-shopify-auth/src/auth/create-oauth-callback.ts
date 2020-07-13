8d2a2e84d26dc491018ac067f736f74242be9abc
Babken Mesropyan <mesropyan.b@gmail.com>
2020-07-13 18:01:48 +0400

    Update packages/koa-shopify-auth/src/auth/create-oauth-callback.ts
    Co-authored-by: Tim Anema <timanema@gmail.com>

--- packages/koa-shopify-auth/src/auth/create-oauth-callback.ts
+++ packages/koa-shopify-auth/src/auth/create-oauth-callback.ts
@@ -56,9 +56,9 @@ export default function createOAuthCallback(config: AuthConfig) {
     const {
       access_token: accessToken,
       associated_user_scope: associatedUserScope,
-      associated_user: associatedUser
+      associated_user: associatedUser,
     } = accessTokenData;
-    
+
     if (ctx.session) {
       ctx.session.shop = shop;
       ctx.session.accessToken = accessToken;
