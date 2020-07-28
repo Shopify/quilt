2cb12453435474e5d8f6e3ec628f18ce116b9eaf
Ismail Syed <isyed867@gmail.com>
2020-07-28 14:16:23 -0400

    Add package-level SK-next configs
    Update TS configs
    
    Remove tracked entrypoints

---
+++ packages/ast-utilities/sewing-kit.config.ts
@@ -0,0 +1,10 @@
+import {createPackage} from '@sewing-kit/config';
+
+import {quiltPackage} from '../../config/sewing-kit';
+
+export default createPackage(pkg => {
+  pkg.entry({root: './src/index'});
+  pkg.entry({name: 'javascript', root: './src/javascript'});
+  pkg.entry({name: 'markdown', root: './src/markdown'});
+  pkg.use(quiltPackage());
+});
