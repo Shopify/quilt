1911eed609af896ebc1930748f994f431c4eab6d
Ismail Syed <isyed867@gmail.com>
2020-07-28 14:09:05 -0400

    Update and move repo tests and testing setup

--- tests/setup/tests.ts
+++ tests/setup/tests.ts
@@ -2,12 +2,12 @@ import addClosest from 'element-closest';
 
 import 'saddle-up/matchers';
 import 'saddle-up/koa-matchers';
-import './matchers';
-import '../packages/ast-utilities/src/matchers';
-import '../packages/react-testing/src/matchers';
-import '../packages/graphql-testing/src/matchers';
+import '../matchers';
+import '../../packages/ast-utilities/src/matchers';
+import '../../packages/react-testing/src/matchers';
+import '../../packages/graphql-testing/src/matchers';
 
-import {destroyAll} from '../packages/react-testing/src/destroy';
+import {destroyAll} from '../../packages/react-testing/src/destroy';
 
 if (typeof window !== 'undefined') {
   addClosest(window);
