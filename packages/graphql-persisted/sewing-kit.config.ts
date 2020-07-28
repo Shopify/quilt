44fea4be1db33cfb742865f4795b1b9bbdfc0e37
Brian Ryu <brian.ryu@shopify.com>
2020-07-28 14:16:23 -0400

    Add package-level SK-next configs
    Update TS configs
    
    Remove tracked entrypoints

---
+++ packages/graphql-persisted/sewing-kit.config.ts
@@ -0,0 +1,9 @@
+import {createPackage} from '@sewing-kit/config';
+
+import {quiltPackage} from '../../config/sewing-kit';
+
+export default createPackage(pkg => {
+  pkg.entry({name: 'koa', root: './src/koa-middleware'});
+  pkg.entry({name: 'apollo', root: './src/apollo'});
+  pkg.use(quiltPackage());
+});
---
+++ packages/react-async/sewing-kit.config.ts
@@ -0,0 +1,9 @@
+import {createPackage} from '@sewing-kit/config';
+
+import {quiltPackage} from '../../config/sewing-kit';
+
+export default createPackage(pkg => {
+  pkg.entry({root: './src/index'});
+  pkg.entry({name: 'testing', root: './src/testing'});
+  pkg.use(quiltPackage({useReact: true}));
+});
