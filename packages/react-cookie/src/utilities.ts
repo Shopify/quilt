2c3068c3b134c86c8ab0733cd91f98c6c79e2833
Brian Ryu <brian.ryu@shopify.com>
2020-08-08 13:11:40 -0400

    Exclude non-suite test files

--- packages/react-cookie/src/utilities.ts
+++ packages/react-cookie/src/utilities.ts
@@ -1,3 +1,27 @@
+import {BrowserCookieManager} from './BrowserCookieManager';
+import {Cookie} from './types';
+
+export function clearCookies() {
+  const cookies = new BrowserCookieManager();
+  const allCookies = cookies.getCookies();
+
+  for (const key of Object.keys(allCookies)) {
+    cookies.removeCookie(key);
+  }
+
+  return cookies;
+}
+
+export function createCookies(initialCookies: Cookie = {}) {
+  const cookies = new BrowserCookieManager();
+
+  for (const [key, value] of Object.entries(initialCookies)) {
+    cookies.setCookie(key, value);
+  }
+
+  return cookies;
+}
+
 export function hasDocumentCookie() {
   return Boolean(
     typeof document === 'object' && typeof document.cookie === 'string',
