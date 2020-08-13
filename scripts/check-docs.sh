d0635b67afad03610207e6bc723fd94a3f6d8016
Ismail Syed <isyed867@gmail.com>
2020-08-12 21:15:24 -0400

    Fix script

--- scripts/check-docs.sh
+++ scripts/check-docs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-test -z "$(git status --porcelain)" || echo "The root README has not been updated. Run 'yarn generate docs' in the root of your Quilt directory and try again."
+if [[ "$(git status --porcelain)" ]]; then
+    echo "The root README has not been updated. Run 'yarn generate docs' in the root of your Quilt directory and try again."
+    exit 1
+fi
