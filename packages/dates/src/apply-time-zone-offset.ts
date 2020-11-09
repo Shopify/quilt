6a509498da1c1e31adea8196fe6e86acc9932632
Kyle Kilbride <kyle.kilbride@shopify.com>
2020-11-09 17:05:37 -0500

    refactor dst check

--- packages/dates/src/apply-time-zone-offset.ts
+++ packages/dates/src/apply-time-zone-offset.ts
@@ -7,12 +7,13 @@ export function applyTimeZoneOffset(
 ) {
   const dateToCheckForDSTOffset = new Date(date);
   dateToCheckForDSTOffset.setHours(2);
+  const needsDSTOffset = date.getTimezoneOffset() !== dateToCheckForDSTOffset.getTimezoneOffset();
+  const dstOffset = needsDSTOffset ? 60 : 0;
 
-  const dstOffsetDiff = date.getTimezoneOffset() - dateToCheckForDSTOffset.getTimezoneOffset();
   const initialOffset = getTimeZoneOffset(date, timeZone1, timeZone2);
   const adjustedDate = new Date(date.valueOf() - initialOffset * 60 * 1000);
   const targetOffset = getTimeZoneOffset(adjustedDate, timeZone1, timeZone2);
-  const offsetDiff = targetOffset - initialOffset + dstOffsetDiff;
+  const offsetDiff = targetOffset - initialOffset - dstOffset;
 
   return new Date(adjustedDate.valueOf() - offsetDiff * 60 * 1000);
 }
