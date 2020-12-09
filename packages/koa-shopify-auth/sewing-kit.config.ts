02460689f285d1b54fcbc96efaf0761ad75b132d
Ismail Syed <isyed867@gmail.com>
2020-12-09 17:07:22 -0500

    More test fixes

--- packages/koa-shopify-auth/sewing-kit.config.ts
+++
@@ -1,8 +0,0 @@
-import {createPackage} from '@sewing-kit/config';
-
-import {quiltPackage} from '../../config/sewing-kit';
-
-export default createPackage(pkg => {
-  pkg.entry({root: './src/index'});
-  pkg.use(quiltPackage());
-});
