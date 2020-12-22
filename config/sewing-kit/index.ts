e27f32396046c6eb666464c2751a37e2096968ba
Ismail Syed <isyed867@gmail.com>
2020-12-22 17:42:25 -0500

    Esbuild cold

--- config/sewing-kit/index.ts
+++ config/sewing-kit/index.ts
@@ -1,4 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'regenerator-runtime/runtime.js';
+import {resolve} from 'path';
+
+import {sync} from 'glob';
 import {
+  PluginApi,
   Package,
   createComposedProjectPlugin,
   createProjectTestPlugin,
@@ -12,12 +18,43 @@ import {} from '@sewing-kit/plugin-jest';
 
 import {addLegacyDecoratorSupport} from './plugin';
 
+const VARIANT = 'esbuildCommonjs';
+declare module '@sewing-kit/hooks' {
+  interface BuildPackageTargetOptions {
+    [VARIANT]: boolean;
+  }
+}
+
 export function quiltPackage({jestEnv = 'jsdom', useReact = false} = {}) {
   return createComposedProjectPlugin<Package>('Quilt.Package', [
     javascript(),
     typescript(),
     useReact && react(),
-    buildFlexibleOutputs(),
+    buildFlexibleOutputs({
+      binaries: true,
+      commonjs: false,
+      esmodules: false,
+      esnext: true,
+      node: true,
+      typescript: true,
+    }),
+
+    createProjectBuildPlugin('Quilt.PackageBuild', ({hooks, api, project}) => {
+      hooks.targets.hook(targets =>
+        targets.map(target =>
+          target.default ? target.add({esbuildCommonjs: true}) : target,
+        ),
+      );
+
+      hooks.steps.hook(steps => {
+        return [
+          ...steps,
+          createEsBuildCompileStep({api, project, format: 'cjs'}),
+          createEsBuildCompileStep({api, project, format: 'esm'}),
+        ];
+      });
+    }),
+
     createProjectBuildPlugin('Quilt.PackageBuild', ({hooks}) => {
       hooks.target.hook(({hooks}) => {
         hooks.configure.hook(hooks => {
@@ -60,4 +97,42 @@ export function quiltPackage({jestEnv = 'jsdom', useReact = false} = {}) {
       });
     }),
   ]);
+}
+
+type Format = 'cjs' | 'esm';
+
+interface EsbuildCompileOptions {
+  readonly api: PluginApi;
+  readonly project: Package;
+  readonly bundle?: boolean;
+  readonly format?: Format;
+  readonly minify?: boolean;
+  readonly sourceMap?: boolean;
+  readonly target?: string[];
+}
+
+function createEsBuildCompileStep({
+  api,
+  project,
+  format = 'cjs',
+}: EsbuildCompileOptions) {
+  return api.createStep(
+    {id: 'esbuild.Compile', label: 'compile with esbuild'},
+    async step => {
+      const sourceRoot = resolve(project.root, 'src');
+      const sourceFiles = sync(resolve(sourceRoot, '**', '*.ts'));
+      const files = sourceFiles.filter(file => !file.includes('.test.'));
+
+      const outputPath = project.fs.buildPath(format === 'esm' ? 'esm' : 'cjs');
+
+      for (const file of files) {
+        await step.exec('node_modules/.bin/esbuild', [
+          file,
+          `--platform=node`,
+          `--outdir=${outputPath}`,
+          `--format=${format}`,
+        ]);
+      }
+    },
+  );
 }
