2526df10e8b0638d55c8d2a18e0ff1cba315ee5f
Ben Scott <ben.scott@shopify.com>
2021-03-16 12:18:50 -0700

    Use named exports in jest transforms

--- packages/graphql-mini-transforms/src/jest-simple.ts
+++ packages/graphql-mini-transforms/src/jest-simple.ts
@@ -8,41 +8,42 @@ import {extractImports} from './document';
 
 const THIS_FILE = readFileSync(__filename);
 
-export const transformer: Transformer = {
-  getCacheKey(fileData, filename) {
-    return createHash('md5')
-      .update(THIS_FILE)
-      .update(fileData)
-      .update(filename)
-      .digest('hex');
-  },
-  process(rawSource) {
-    const {imports, source} = extractImports(rawSource);
+export const getCacheKey: Transformer['getCacheKey'] = function getCacheKey(
+  fileData,
+  filename,
+) {
+  return createHash('md5')
+    .update(THIS_FILE)
+    .update(fileData)
+    .update(filename)
+    .digest('hex');
+};
 
-    const utilityImports = `
+export const process: Transformer['process'] = function process(rawSource) {
+  const {imports, source} = extractImports(rawSource);
+
+  const utilityImports = `
       var {print, parse} = require('graphql');
       var {cleanDocument, toSimpleDocument} = require(${JSON.stringify(
         `${__dirname}/document.js`,
       )});
     `;
 
-    const importSource = imports
-      .map(
-        (imported, index) =>
-          `var importedDocument${index} = require(${JSON.stringify(
-            imported,
-          )});`,
-      )
-      .join('\n');
+  const importSource = imports
+    .map(
+      (imported, index) =>
+        `var importedDocument${index} = require(${JSON.stringify(imported)});`,
+    )
+    .join('\n');
 
-    const appendDefinitionsSource = imports
-      .map(
-        (_, index) =>
-          `document.definitions.push.apply(document.definitions, parse(importedDocument${index}.source).definitions);`,
-      )
-      .join('\n');
+  const appendDefinitionsSource = imports
+    .map(
+      (_, index) =>
+        `document.definitions.push.apply(document.definitions, parse(importedDocument${index}.source).definitions);`,
+    )
+    .join('\n');
 
-    return `
+  return `
       ${utilityImports}
       ${importSource}
 
@@ -52,7 +53,4 @@ export const transformer: Transformer = {
 
       module.exports = toSimpleDocument(cleanDocument(document, {removeUnused: false}));
     `;
-  },
 };
-
-module.exports = transformer;
