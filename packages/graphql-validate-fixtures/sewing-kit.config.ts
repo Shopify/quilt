71abe22eeedcfba6c5610f5c1de691427303d912
Ben Scott <227292+BPScott@users.noreply.github.com>
2021-03-23 13:09:25 -0700

    Fix bin file generation (#1798)
    graphql-typescript-definitions and graphql-validate-fixture have
    binarys. Instead of manually writing the bin file content, we can
    get sewing-kit to generate these bin folders for us
    
    - Deletes packages/*/bin (only affects graphql-typescript-definitions
      and graphql-validate-fixtures
    - Add packages/*/bin to gitignore as this folder will be generated by SK
    - Add pkg.binary() definitions for packages that have binarys

--- packages/graphql-validate-fixtures/sewing-kit.config.ts
+++ packages/graphql-validate-fixtures/sewing-kit.config.ts
@@ -4,5 +4,6 @@ import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage(pkg => {
   pkg.entry({root: './src/index'});
+  pkg.binary({name: 'graphql-validate-fixtures', root: './src/cli'});
   pkg.use(quiltPackage());
 });
