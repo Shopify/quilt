9f80583bddbed5ff904e46c83185234572b045d9
Ben Scott <ben.scott@shopify.com>
2021-04-07 17:11:24 -0700

    Add runtimes to all packages

--- packages/graphql-typed/sewing-kit.config.ts
+++ packages/graphql-typed/sewing-kit.config.ts
@@ -3,6 +3,7 @@ import {createPackage, Runtime} from '@sewing-kit/config';
 import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage(pkg => {
+  pkg.runtimes(Runtime.Browser, Runtime.Node);
   pkg.entry({root: './src/index'});
   pkg.use(quiltPackage());
 });
