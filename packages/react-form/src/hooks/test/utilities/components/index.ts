5101ddaa7d5c9e18e4c3042799b62fa9b4129cea
oluwatimio <oluwatimi.owoturo@shopify.com>
2021-04-08 20:28:13 -0400

    reset function and dirty state
    test baselist
    
    Update baselist.test.tsx
    
    lint fix and additional tests
    
    update tests
    
    Update CHANGELOG.md
    
    Update README.md
    
    Update CHANGELOG.md
    
    add dynamic list to the form
    
    update comments
    
    Update form.ts
    
    add check for input since theres a possibility of adding undefined input dynamicListFields
    
    Update baselist.ts
    
    Update baselist.ts
    
    added tests to form
    
    Update form.test.tsx
    
    fixed reset recreating on each render
    
    Update listdirty.ts
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    added DynamicListBag
    
    Update packages/react-form/README.md
    
    Co-authored-by: Sylvain Hamann <sylvain.hamann@shopify.com>
    
    Update packages/react-form/README.md
    
    Co-authored-by: Sylvain Hamann <sylvain.hamann@shopify.com>
    
    Update README.md
    
    Update baselist.ts
    
    docs
    
    used lazy ref
    
    fix docs
    
    more improvement to docs
    
    return default bag on undefined dynamic list
    
    Update form.test.tsx
    
    rebase gone bad fix
    
    improve dynamicLists typing (#1848)
    
    * improve dynamicLists typing
    
    * revert to initial tests
    
    * mock console.error to clean logs
    
    * add dirty tests for dynamic lists
    
    * update form tests
    
    * remove useless file
    
    Update dynamiclist.test.tsx

---
+++ packages/react-form/src/hooks/test/utilities/components/index.ts
@@ -0,0 +1,4 @@
+export {FormWithDynamicVariantList} from './FormWithDynamicVariantList';
+export {ProductForm} from './ProductForm';
+export {TextField} from './TextField';
+export * from './types';
