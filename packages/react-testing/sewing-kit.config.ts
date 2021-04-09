bd5934dd40ce76027b260aab35b20441a1500ecb
Ben Scott <227292+BPScott@users.noreply.github.com>
2021-04-09 14:40:06 -0700

    Add build runtime and remove outdated config (#1829)
    * Remove outdated build script and dependencies on tslib
    * Add runtimes to all packages
    * Update runtimes of node-only packages

--- packages/react-testing/sewing-kit.config.ts
+++ packages/react-testing/sewing-kit.config.ts
@@ -1,8 +1,9 @@
-import {createPackage} from '@sewing-kit/config';
+import {createPackage, Runtime} from '@sewing-kit/config';
 
 import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage(pkg => {
+  pkg.runtimes(Runtime.Browser, Runtime.Node);
   pkg.entry({root: './src/index'});
   pkg.entry({name: 'matchers', root: './src/matchers'});
   pkg.use(quiltPackage({useReact: true}));
