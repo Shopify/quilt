7068de6b719c8e112af56fddfefe1ccb1846e714
Stephen Hukish <dahukish@users.noreply.github.com>
2021-04-22 16:23:37 -0400

    Revert "Add core-js polyfills for node 10 CI breakages"

--- packages/graphql-tool-utilities/src/polyfills.ts
+++ packages/graphql-tool-utilities/src/polyfills.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line no-warning-comments
-// TODO: Remove once we kill NodeJs 10 support
 // apollo-codegen uses Object.entries and Array.flatmap
 import 'core-js/modules/es.object.entries';
 import 'core-js/modules/es.array.flat-map';
