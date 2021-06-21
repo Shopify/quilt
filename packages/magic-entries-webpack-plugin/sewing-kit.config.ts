81e88cc37ad59750dcc687e8f8b863d8bacfb353
Victor Sumner <victor.sumner@shopify.com>
2021-06-21 10:45:34 -0400

    Import setimmediate

--- packages/magic-entries-webpack-plugin/sewing-kit.config.ts
+++ packages/magic-entries-webpack-plugin/sewing-kit.config.ts
@@ -2,8 +2,8 @@ import {createPackage, Runtime} from '@sewing-kit/config';
 
 import {quiltPackage} from '../../config/sewing-kit';
 
-export default createPackage(pkg => {
+export default createPackage((pkg) => {
   pkg.runtimes(Runtime.Node);
   pkg.entry({root: './src/index'});
-  pkg.use(quiltPackage());
+  pkg.use(quiltPackage({jestEnv: 'node'}));
 });
