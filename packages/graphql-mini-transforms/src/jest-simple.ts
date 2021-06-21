3ca51eb5dd04fc2e40eb91994c7a2b9a0fc0a77e
Victor Sumner <victor.sumner@shopify.com>
2021-06-21 16:46:44 -0400

    export default transformer

--- packages/graphql-mini-transforms/src/jest-simple.ts
+++ packages/graphql-mini-transforms/src/jest-simple.ts
@@ -8,49 +8,50 @@ import {extractImports} from './document';
 
 const THIS_FILE = readFileSync(__filename);
 
-export const getCacheKey: Transformer['getCacheKey'] = function getCacheKey(
-  fileData,
-  filename,
-) {
-  return createHash('md5')
-    .update(THIS_FILE)
-    .update(fileData)
-    .update(filename)
-    .digest('hex');
+const transformer: Transformer = {
+  getCacheKey(fileData, filename) {
+    return createHash('md5')
+      .update(THIS_FILE)
+      .update(fileData)
+      .update(filename)
+      .digest('hex');
+  },
+  process(rawSource) {
+    const {imports, source} = extractImports(rawSource);
+
+    const utilityImports = `
+        var {print, parse} = require('graphql');
+        var {cleanDocument, toSimpleDocument} = require(${JSON.stringify(
+          `${__dirname}/document.js`,
+        )});
+      `;
+
+    const importSource = imports
+      .map(
+        (imported, index) =>
+          `var importedDocument${index} = require(${JSON.stringify(
+            imported,
+          )});`,
+      )
+      .join('\n');
+
+    const appendDefinitionsSource = imports
+      .map(
+        (_, index) =>
+          `document.definitions.push.apply(document.definitions, parse(importedDocument${index}.source).definitions);`,
+      )
+      .join('\n');
+
+    return `
+        ${utilityImports}
+        ${importSource}
+
+        var document = ${JSON.stringify(parse(source))};
+
+        ${appendDefinitionsSource}
+
+        module.exports = toSimpleDocument(cleanDocument(document, {removeUnused: false}));
+      `;
+  },
 };
-
-export const process: Transformer['process'] = function process(rawSource) {
-  const {imports, source} = extractImports(rawSource);
-
-  const utilityImports = `
-      var {print, parse} = require('graphql');
-      var {cleanDocument, toSimpleDocument} = require(${JSON.stringify(
-        `${__dirname}/document.js`,
-      )});
-    `;
-
-  const importSource = imports
-    .map(
-      (imported, index) =>
-        `var importedDocument${index} = require(${JSON.stringify(imported)});`,
-    )
-    .join('\n');
-
-  const appendDefinitionsSource = imports
-    .map(
-      (_, index) =>
-        `document.definitions.push.apply(document.definitions, parse(importedDocument${index}.source).definitions);`,
-    )
-    .join('\n');
-
-  return `
-      ${utilityImports}
-      ${importSource}
-
-      var document = ${JSON.stringify(parse(source))};
-
-      ${appendDefinitionsSource}
-
-      module.exports = toSimpleDocument(cleanDocument(document, {removeUnused: false}));
-    `;
-};
+export default transformer;
