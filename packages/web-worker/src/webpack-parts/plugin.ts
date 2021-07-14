cf2cf2a6c79618df1414106ec25b8edf8857abdc
Ates Goral <ates@magnetiq.com>
2021-07-14 13:58:06 -0400

    Update web-worker plugin

--- packages/web-worker/src/webpack-parts/plugin.ts
+++ packages/web-worker/src/webpack-parts/plugin.ts
@@ -1,26 +1,25 @@
 import VirtualModulesPlugin from 'webpack-virtual-modules';
+import type {WebpackPluginInstance, Compiler} from 'webpack';
 
 import {PLUGIN} from '../common';
 
-type Plugin = import('webpack').Plugin;
-
 interface Options {
   globalObject?: string;
-  plugins?: Plugin[];
+  plugins?: WebpackPluginInstance[];
 }
 
-export class WebWorkerPlugin implements Plugin {
+export class WebWorkerPlugin implements WebpackPluginInstance {
   static isInstance(value: unknown): value is WebWorkerPlugin {
     return value != null && (value as WebWorkerPlugin)[PLUGIN];
   }
 
-  public readonly virtualModules = new VirtualModulesPlugin({});
+  public readonly virtualModules = new VirtualModulesPlugin();
   public workerId = 0;
   private readonly [PLUGIN] = true;
 
   constructor(public readonly options: Options = {}) {}
 
-  apply(compiler: import('webpack').Compiler) {
+  apply(compiler: Compiler) {
     this.virtualModules.apply(compiler);
   }
 }
