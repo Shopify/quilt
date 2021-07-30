a7c5bda0b33a245189a9fd34ced2bedfcf8df353
Ben Scott <ben.scott@shopify.com>
2021-07-29 18:56:22 -0700

    config tweak

--- config/sewing-kit/index.ts
+++ config/sewing-kit/index.ts
@@ -12,19 +12,17 @@ export function quiltPackage({
   jestTestRunner = 'jest-circus',
   polyfill = true,
 } = {}) {
+  // The babel preset polyfills by default, if we don't want polyfilling to
+  // occur we need to turn some options off
+  const polyfillOptions = polyfill ? {} : {useBuiltIns: false, corejs: false};
+
   return createComposedProjectPlugin<Package>('Quilt.Package', [
     babel({
       config: {
         presets: [
           [
             '@shopify/babel-preset',
-            {
-              typescript: true,
-              react: true,
-              // The preset polyfills by default, we need explicit options to
-              // disable that
-              ...(polyfill ? {} : {useBuiltIns: false, corejs: false}),
-            },
+            {typescript: true, react: true, ...polyfillOptions},
           ],
         ],
       },
@@ -36,18 +34,12 @@ export function quiltPackage({
     createProjectTestPlugin('Quilt.PackageTest', ({hooks}) => {
       hooks.configure.hook((hooks) => {
         hooks.jestEnvironment?.hook(() => jestEnv);
+        hooks.jestTestRunner?.hook(() => jestTestRunner);
 
         hooks.jestTransforms?.hook((transforms) => ({
           ...transforms,
           '\\.(gql|graphql)$': 'jest-transform-graphql',
         }));
-
-        hooks.jestConfig?.hook((jestConfig) => {
-          return {
-            ...jestConfig,
-            testRunner: jestTestRunner,
-          };
-        });
 
         hooks.jestWatchIgnore?.hook((patterns) => [
           ...patterns,
