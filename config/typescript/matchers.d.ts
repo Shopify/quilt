81b16c478db2ace5ddf212a414b9453d1ac7e4ef
Peter Hamilton <peter.hamilton@shopify.com>
2021-09-09 16:53:38 -0400

    Replace setup.ts files with a d.ts reference.

---
+++ config/typescript/matchers.d.ts
@@ -0,0 +1,30 @@
+import {ComponentType, Context as ReactContext} from 'react';
+import type {} from 'saddle-up/matchers';
+import type {} from 'saddle-up/koa-matchers';
+
+// @shopify/react-testing/matchers
+declare type PropsFromNode<T> = T extends Node<infer U> ? U : never;
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    interface Matchers<R, T = {}> {
+      toHaveReactProps(props: Partial<PropsFromNode<T>>): void;
+      toHaveReactDataProps(data: {[key: string]: string}): void;
+      toContainReactComponent<Type extends string | ComponentType<any>>(
+        type: Type,
+        props?: Partial<PropsFor<Type>>,
+      ): void;
+      toContainReactComponentTimes<Type extends string | ComponentType<any>>(
+        type: Type,
+        times: number,
+        props?: Partial<PropsFor<Type>>,
+      ): void;
+      toProvideReactContext<Type>(
+        context: ReactContext<Type>,
+        value?: Type,
+      ): void;
+      toContainReactText(text: string): void;
+      toContainReactHtml(text: string): void;
+    }
+  }
+}
--- packages/react-tracking-pixel/src/tests/setup.ts
+++
@@ -1,2 +0,0 @@
-// Import jest matchers for the purposes of type checking. sewing-kit performs a similar setup before running jest tests.
-import '@shopify/react-testing/matchers';
