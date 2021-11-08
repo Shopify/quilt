59000af894670e41c6261e09599ff477391db0c2
Matt Seccafien <mseccafien@gmail.com>
2021-11-08 22:46:28 +0100

    No duplicates in addComponentProps

--- packages/ast-utilities/src/javascript/addComponentProps/addComponentProps.ts
+++ packages/ast-utilities/src/javascript/addComponentProps/addComponentProps.ts
@@ -1,9 +1,14 @@
 import * as traverse from '@babel/traverse';
 import * as t from '@babel/types';
 
+interface Options {
+  noDuplicates: boolean;
+}
+
 export default function addComponentProps(
   props: {name: string; value: t.StringLiteral | t.Identifier}[],
   component: string,
+  options: Options = {noDuplicates: true},
 ) {
   return {
     JSXElement(path: traverse.NodePath<t.JSXElement>) {
@@ -13,6 +18,28 @@ export default function addComponentProps(
         t.isJSXIdentifier(openingElement.name) &&
         openingElement.name.name === component
       ) {
+        const hasExistingProp = openingElement.attributes.filter((attr) =>
+          Boolean(
+            props.filter((prop) => {
+              if (
+                t.isJSXAttribute(attr) &&
+                t.isJSXExpressionContainer(attr.value) &&
+                t.isIdentifier(attr.value.expression) &&
+                t.isIdentifier(prop.value)
+              ) {
+                return (
+                  prop.name === attr.name.name &&
+                  prop.value.name === attr.value.expression.name
+                );
+              }
+            }).length,
+          ),
+        );
+
+        if (options.noDuplicates && hasExistingProp.length) {
+          return;
+        }
+
         props.forEach((prop) => {
           const value = t.isStringLiteral(prop.value)
             ? prop.value
