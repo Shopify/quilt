4adfe9771f309383c76821c47633c8b450e56343
Ryan Keller <ryan.keller@shopify.com>
2022-01-11 08:43:50 -0500

    fix: interceptGql is now registed in index, fix typings

--- packages/cypress-graphql/src/commands/intereceptGql.ts
+++ packages/cypress-graphql/src/commands/intereceptGql.ts
@@ -5,48 +5,44 @@ import {
 
 import {hasOperationName} from '../utils/hasOperationName';
 
-Cypress.Commands.add(
-  'interceptGql',
-  (
-    knownOperations: (
-      | string
-      | [string, StaticResponse | HttpResponseInterceptor]
-    )[],
-  ) => {
-    // Create a single cypress route for all GQL queries
-    Cypress.log({
-      name: 'Intercept GraphQL',
-      displayName: 'interceptGql',
-      message: knownOperations.map((op) => (Array.isArray(op) ? op[0] : op)),
-      consoleProps: () => knownOperations,
-    });
-    cy.intercept('POST', '**/graphql', (req) => {
-      // Define sub-routes based on the graphql operation name
-      for (const operation of knownOperations) {
-        const operationName = Array.isArray(operation)
-          ? operation[0]
-          : operation;
-        const responseInterceptor = Array.isArray(operation)
-          ? operation[1]
-          : undefined;
-        if (hasOperationName(req, operationName)) {
-          // NOTE: cannot use cy.log in here due to circular logic
+export const interceptGql = (
+  graphqlApiUrl: string,
+  knownOperations: (
+    | string
+    | [string, StaticResponse | HttpResponseInterceptor]
+  )[],
+) => {
+  // Create a single cypress route to intercept GQL queries to graphqlApiUrl
+  Cypress.log({
+    name: 'Intercept GraphQL',
+    displayName: 'interceptGql',
+    message: knownOperations.map((op) => (Array.isArray(op) ? op[0] : op)),
+    consoleProps: () => knownOperations,
+  });
+  cy.intercept('POST', graphqlApiUrl, (req) => {
+    // Define sub-routes based on the graphql operation name
+    for (const operation of knownOperations) {
+      const operationName = Array.isArray(operation) ? operation[0] : operation;
+      const responseInterceptor = Array.isArray(operation)
+        ? operation[1]
+        : undefined;
+      if (hasOperationName(req, operationName)) {
+        // NOTE: cannot use cy.log in here due to circular logic
+        // eslint-disable-next-line no-console
+        console.log(`Request fired for @${operationName}:`, req);
+        // set an alias automatically for practical use e.g. cy.wait('@QueryName')
+        req.alias = operationName;
+        // Override the response with mock data, if provided
+        if (typeof responseInterceptor !== 'undefined') {
           // eslint-disable-next-line no-console
-          console.log(`Request fired for @${operationName}:`, req);
-          // set an alias automatically for practical use e.g. cy.wait('@QueryName')
-          req.alias = operationName;
-          // Override the response with mock data, if provided
-          if (typeof responseInterceptor !== 'undefined') {
-            // eslint-disable-next-line no-console
-            console.warn(
-              `Response intercepted for ${operationName}:`,
-              responseInterceptor,
-            );
-            req.reply(responseInterceptor);
-          }
-          return undefined;
+          console.warn(
+            `Response intercepted for ${operationName}:`,
+            responseInterceptor,
+          );
+          req.reply(responseInterceptor);
         }
+        return undefined;
       }
-    }).as('GraphQL');
-  },
-);
+    }
+  }).as('GraphQL');
+};
--- packages/cypress-graphql/src/index.ts
+++ packages/cypress-graphql/src/index.ts
@@ -1,8 +1,48 @@
-// / <reference types="cypress" />
-// / <reference path="index.d.ts" />
+import {
+  StaticResponse,
+  HttpResponseInterceptor,
+} from 'cypress/types/net-stubbing';
 
-import './commands/intereceptGql';
+// Import custom commands
+import {interceptGql} from './commands/intereceptGql';
 
+// Re-export utility functions and libs
 export * from './utils/aliasMutation';
 export * from './utils/aliasQuery';
 export * from './utils/hasOperationName';
+
+/**
+ * register custom commands automatically
+ * (this happens at the global scope by design)
+ * @see https://docs.cypress.io/guides/tooling/typescript-support#Types-for-custom-commands
+ **/
+Cypress.Commands.add('interceptGql', interceptGql);
+
+/**
+ * Augment Cypress's interfaces so they include typings for custom commands
+ * @see https://docs.cypress.io/guides/tooling/typescript-support#Types-for-custom-commands
+ */
+
+/* eslint-disable @typescript-eslint/no-namespace */
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      //
+      /**
+       * interceptGql will log and intercept GQL queries/mutations based on the operationname
+       * Said requests can be awaited and their responses may be tested like any cypress route
+       * @see https://docs.cypress.io/api/commands/intercept
+       * @future If this works well, add as package for https://github.com/Shopify/quilt
+       * @param knownOperations list of known queries/mutations (plus response intercepts, if needed)
+       */
+      interceptGql(
+        graphqlApiUrl: string,
+        knownOperations: (
+          | string
+          | [string, StaticResponse | HttpResponseInterceptor]
+        )[],
+      ): Chainable<undefined>;
+    }
+  }
+}
+/* eslint-enable @typescript-eslint/no-namespace */
