6e614798871e5808630541f956bf347011b0adbb
Kevin Meinhardt <kevin.meinhardt@shopify.com>
2022-03-03 15:21:25 +0100

    fix(): fix bugs found in DeepOmit, add more tests

--- packages/useful-types/test-d/types.test-d.ts
+++ packages/useful-types/test-d/types.test-d.ts
@@ -111,45 +111,128 @@ expectNotType<IfEmptyObject<boolean, true>>(true);
  * DeepOmit
  */
 
-expectAssignable<DeepOmit<string, 'test'>>('string');
-
+/**
+ * should return primitives unaltered
+ */
+expectAssignable<DeepOmit<string, '__typename'>>('string');
+expectNotAssignable<DeepOmit<string, 'string'>>(undefined);
+expectAssignable<DeepOmit<string, string>>('string');
 interface Obj {
   __typename: string;
   foo: string;
-  bar?: {
+  bar: {
     __typename: string;
     baz: string;
   };
-  list?: Obj[];
 }
 
+/**
+ * should omit key at any level of nesting
+ * will recurse on {__typename: string; baz: string;}
+ * should omit __typename
+ */
 expectAssignable<DeepOmit<Obj, '__typename'>>({
   foo: 'string',
   bar: {
-    /**
-     * This should error:
-     * DeepOmit does not seem to be recursing on nested types
-     */
-    __typename: 'Nested',
+    baz: 'string',
+  },
+});
+
+expectAssignable<DeepOmit<Obj, 'baz'>>({
+  __typename: 'string',
+  foo: 'string',
+  bar: {
+    __typename: 'string',
+  },
+});
+
+expectNotAssignable<DeepOmit<Obj, '__typename'>>({
+  __typename: 'string',
+});
+
+/**
+ * DeepOmit<Obj>
+ * will recurse on {__typename: string; baz: string;}
+ * should omit __typename
+ */
+
+expectNotAssignable<DeepOmit<Obj, '__typename'>>({
+  __typename: 'string',
+  foo: 'string',
+  bar: {
     baz: 'string',
   },
 });
 
 expectNotAssignable<DeepOmit<Obj, '__typename'>>({
   foo: 'string',
-  list: [
-    {
-      /**
-       * this should NOT error:
-       * DeepOmit also seems to not respect existing optional properties
-       */
-      bar: undefined,
-      list: undefined,
-    },
-  ],
+  bar: {
+    __typename: 'string',
+    baz: 'string',
+  },
 });
 
-expectNotType<DeepOmit<Obj, '__typename'>>({
+type NullableUnionArray = DeepOmit<
+  {bar: (string | number)[] | null; __typename: string},
+  '__typename'
+>;
+
+/**
+ * will recurse on (string | number)[] | null
+ * we want to allow null
+ */
+expectAssignable<NullableUnionArray>({bar: null});
+
+/**
+ * will recurse on (string | number)[] | null
+ * we want to allow empty or valid arrays
+ */
+expectAssignable<NullableUnionArray>({bar: []});
+expectAssignable<NullableUnionArray>({bar: ['string']});
+expectAssignable<NullableUnionArray>({bar: [2]});
+expectAssignable<NullableUnionArray>({bar: ['string', 2]});
+
+/**
+ * should not allow invalid array elements
+ */
+expectNotAssignable<NullableUnionArray>({bar: [undefined]});
+expectNotAssignable<NullableUnionArray>({bar: [null]});
+
+expectNotAssignable<NullableUnionArray>({bar: undefined});
+
+/**
+ * DeepOmit of type {bar: (string| number)[]}
+ * will recurse on (string | number)[]
+ * will recurse on (string| number)
+ * we want to allow string or number
+ */
+expectAssignable<DeepOmit<string | number, 'toString'>>('string');
+expectAssignable<DeepOmit<string | number, 'toString'>>(2);
+expectNotAssignable<DeepOmit<string | number, 'toString'>>(null);
+expectNotAssignable<DeepOmit<string | number, 'toString'>>(undefined);
+
+interface RespectOptionalProps {
+  __typename: string;
+  foo: string;
+  bar?: {
+    __typename: string;
+    baz: number;
+  };
+}
+
+/**
+ * respects optional bar
+ */
+expectAssignable<DeepOmit<RespectOptionalProps, '__typename'>>({
+  foo: 'string',
+});
+
+expectAssignable<DeepOmit<RespectOptionalProps, '__typename'>>({
+  foo: 'string',
+  bar: undefined,
+});
+
+expectNotAssignable<DeepOmit<RespectOptionalProps, '__typename'>>({
   __typename: 'string',
   foo: 'string',
 });
