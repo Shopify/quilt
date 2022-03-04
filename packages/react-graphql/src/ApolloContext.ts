f39ce3fa295030bfa033ed3d528904ebec6dd796
Emily Ye <emily.ye@shopify.com>
2022-03-04 15:50:48 -0500

    remove extra spacing

--- packages/react-graphql/src/ApolloContext.ts
+++ packages/react-graphql/src/ApolloContext.ts
@@ -1,10 +1,10 @@
 import React from 'react';
-import { ApolloClient } from '@apollo/client';
-import { DocumentNode } from 'graphql-typed';
+import {ApolloClient} from '@apollo/client';
+import {DocumentNode} from 'graphql-typed';
 
 export interface ApolloContextValue<CacheShape = any> {
   client?: ApolloClient<CacheShape>;
-  operations?: Map<string, { query: DocumentNode; variables: any }>;
+  operations?: Map<string, {query: DocumentNode; variables: any}>;
 }
 
 export const ApolloContext = React.createContext<
