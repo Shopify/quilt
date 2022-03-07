900677a77521c07c9ba96a28b25074f29f881fdd
Emily Ye <emily.ye@shopify.com>
2022-03-07 10:41:41 -0500

    update react-graphql-universal-provider to Apollo

--- packages/react-graphql-universal-provider/src/GraphQLUniversalProvider.tsx
+++ packages/react-graphql-universal-provider/src/GraphQLUniversalProvider.tsx
@@ -31,7 +31,14 @@ export function GraphQLUniversalProvider<
   const requestID = useRequestHeader('X-Request-ID');
 
   const [client, ssrLink] = useLazyRef<
+<<<<<<< HEAD
     [ApolloClient<any>, ReturnType<typeof createSsrExtractableLink> | undefined]
+=======
+    [
+      import('@apollo/client').ApolloClient<any>,
+      ReturnType<typeof createSsrExtractableLink> | undefined,
+    ]
+>>>>>>> 93efd47a6 (update react-graphql-universal-provider to Apollo)
   >(() => {
     const server = isServer();
 
