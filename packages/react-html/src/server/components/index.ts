12874a05262596574beeacbfd335299557191929
Victor Sumner <victor.sumner@shopify.com>
2022-03-31 09:19:53 -0400

    correct exports

--- packages/react-html/src/server/components/index.ts
+++ packages/react-html/src/server/components/index.ts
@@ -1,8 +1,8 @@
 export {default as Serialize} from './Serialize';
 export {default as Html} from './Html';
 export type {HtmlProps} from './Html';
-export {default as Script} from './Script';
+export {Script} from './Script';
 export type {Props as ScriptProps} from './Script';
-export {default as InlineStyle} from './InlineStyle';
-export {default as Stylesheet} from './Stylesheet';
+export {InlineStyle} from './InlineStyle';
+export {Stylesheet} from './Stylesheet';
 export type {Props as StylesheetProps} from './Stylesheet';
