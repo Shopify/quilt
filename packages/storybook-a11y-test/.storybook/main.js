345086cf7337cc0aa471257772195446fe473e7a
Ben Scott <ben.scott@shopify.com>
2022-04-06 09:50:38 -0700

    Remove addons and simplify babel config

--- packages/storybook-a11y-test/.storybook/main.js
+++ packages/storybook-a11y-test/.storybook/main.js
@@ -1,22 +1,11 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-postcss'],
+  addons: [],
   framework: '@storybook/react',
   features: {
     storyStoreV7: true,
   },
   babel: (config) => {
-    // This is to change babel-preset-env and we have to do this manually to prevent polluting the package.json
-    const presetEnv = config.presets.find((preset) =>
-      preset[0].includes('@babel/preset-env'),
-    );
-    presetEnv[1] = {
-      useBuiltIns: 'usage',
-      corejs: 3,
-      targets: '> 0.25%, not dead',
-      loose: true,
-      shippedProposals: true,
-    };
-    return config;
+    return {targets: 'last 3 chrome versions', ...config};
   },
 };
