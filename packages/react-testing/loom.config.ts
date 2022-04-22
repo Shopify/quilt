0af75d15a1a7f00676d99477f90701192f74d21f
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2022-04-22 09:46:49 -0400

    Configure loom to pickup test environment
    Loom typically looks for setupFiles under tests/setup/environment.* but
    we have ours under the additional sub-directory of src/ because of the
    way that react-testing lays out its own tests.

--- packages/react-testing/loom.config.ts
+++ packages/react-testing/loom.config.ts
@@ -1,4 +1,4 @@
-import {createPackage} from '@shopify/loom';
+import {createPackage, createProjectPlugin} from '@shopify/loom';
 
 import {quiltPackage} from '../../config/loom';
 
@@ -6,4 +6,18 @@ export default createPackage((pkg) => {
   pkg.entry({root: './src/index.ts'});
   pkg.entry({name: 'matchers', root: './src/matchers/index.ts'});
   pkg.use(quiltPackage());
+  pkg.use(jestAdjustments());
 });
+
+function jestAdjustments() {
+  return createProjectPlugin('JestAdjustments', ({tasks: {test}}) => {
+    test.hook(({hooks}) => {
+      hooks.configure.hook((configuration) => {
+        configuration.jestSetupFiles?.hook((files) => [
+          ...files,
+          './src/tests/setup/environment.ts',
+        ]);
+      });
+    });
+  });
+}
