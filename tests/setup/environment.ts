311beb16c35d2562fc3247b1a917b5b9bfe279c3
Victor Sumner <victor.sumner@shopify.com>
2022-05-19 11:25:58 -0400

    Update tests

--- tests/setup/environment.ts
+++ tests/setup/environment.ts
@@ -1,11 +1,6 @@
 import './console-wrapper';
 import 'cross-fetch';
 
-// Ensure that ICU data is loaded for tests so that Intl methods respect locale
-// and act like they do in the browser
-// eslint-disable-next-line no-process-env
-process.env.NODE_ICU_DATA = 'node_modules/full-icu';
-
 if (Intl.PluralRules == null) {
   require('intl-pluralrules');
 }
