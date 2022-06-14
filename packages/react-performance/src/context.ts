aeb614f770d4e579ef5c8d55ff9bdf0fe444e7fc
Bret Little <bret.little@shopify.com>
2022-06-14 14:39:12 -0400

    Fix: support netlify edge functions
    Netlify Edge Functions have a global `window` but not a global `document`

--- packages/react-performance/src/context.ts
+++ packages/react-performance/src/context.ts
@@ -2,5 +2,5 @@ import React from 'react';
 import {Performance} from '@shopify/performance';
 
 export const PerformanceContext = React.createContext<Performance | null>(
-  typeof window === 'undefined' ? null : new Performance(),
+  typeof document === 'undefined' ? null : new Performance(),
 );
