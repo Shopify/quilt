23152a41ba51c7da630ab95c36cbc6bdfccb966e
Stewart Parry <stusaysyo@gmail.com>
2022-06-30 09:34:57 -0400

    Swap the random chooseNull result

--- packages/graphql-fixtures/src/utilities.ts
+++ packages/graphql-fixtures/src/utilities.ts
@@ -1,7 +1,7 @@
 import faker from '@faker-js/faker/locale/en';
 
 export function chooseNull() {
-  return faker.datatype.boolean();
+  return !faker.datatype.boolean();
 }
 
 export function randomFromArray<T>(array: T[] | ReadonlyArray<T>) {
