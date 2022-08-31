5200a1547dbd698507a2da2e0fb7b320b3a8b338
Emile Bergeron <emileber@users.noreply.github.com>
2022-08-31 16:32:13 -0400

    Export Bugsnag's Client type and Config type (#2394)

--- packages/react-bugsnag/src/client.ts
+++ packages/react-bugsnag/src/client.ts
@@ -1,6 +1,9 @@
 import bugsnag, {Config} from '@bugsnag/js';
 import BugsnagPluginReact from '@bugsnag/plugin-react';
 
+export type {Client} from '@bugsnag/js';
+export type {Config};
+
 export function createBugsnagClient(options: Config) {
   // We could alternatively use `start` here, the only difference between them is that `start` also sets a property on the Bugsnag global
   return bugsnag.createClient({
