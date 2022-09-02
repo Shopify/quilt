79dedd079bb48995599884367421bbf076abdddc
Ben Scott <ben.scott@shopify.com>
2022-09-01 17:21:33 -0700

    Fix types of GraphQL's cacheOptions

--- packages/graphql-testing/src/graphql-controller.ts
+++ packages/graphql-testing/src/graphql-controller.ts
@@ -2,9 +2,8 @@ import {
   ApolloClient,
   ApolloLink,
   GraphQLRequest,
-  InMemoryCacheConfig,
   InMemoryCache,
-  PossibleTypesMap,
+  InMemoryCacheConfig,
 } from '@apollo/client';
 
 import {MockLink, InflightLink} from './links';
@@ -13,7 +12,7 @@ import {operationNameFromFindOptions} from './utilities';
 import {GraphQLMock, MockRequest, FindOptions} from './types';
 
 export interface Options {
-  possibleTypes?: PossibleTypesMap;
+  possibleTypes?: InMemoryCacheConfig['possibleTypes'];
   cacheOptions?: InMemoryCacheConfig;
   links?: ApolloLink[];
   assumeImmutableResults?: boolean;
