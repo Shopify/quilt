1121b5da2787e1dabfffc5938154e782c752bebb
Mateus Ferreira <mtsferreirasilva@gmail.com>
2022-09-25 18:41:12 -0400

    Set a generic type for the variant value

--- packages/css-utilities/src/tests/variation-name.test.ts
+++ packages/css-utilities/src/tests/variation-name.test.ts
@@ -2,15 +2,16 @@ import {variationName} from '../variation-name';
 
 describe('variationName()', () => {
   it('returns name plus variation value with the first letter capitalized when value is string', () => {
+    type Variants = 'white' | 'black';
     const name = 'color';
     const value = 'white';
 
-    expect(variationName(name, value)).toBe('colorWhite');
+    expect(variationName<Variants>(name, value)).toBe('colorWhite');
   });
 
   it('returns name plus variation value when value is number', () => {
     const name = 'size';
-    const value = '123';
+    const value = 123;
 
     expect(variationName(name, value)).toBe('size123');
   });
