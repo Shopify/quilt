43eea3a2a2b0a02bf6a6e4825e930665684c48ff
Ben Scott <ben.scott@shopify.com>
2023-02-08 16:51:45 -0800

    Remove unneeded test setup
    - Element.closest is built into jsdom so no need for the polyfill
    - Intl.PluralRules in node supports all languages as of node 13

--- tests/setup/environment.ts
+++ tests/setup/environment.ts
@@ -5,7 +5,3 @@ import {TextEncoder, TextDecoder} from 'util';
 // our node-fetch polyfill is old, this isn't supported until whatwg-url is ^10.0.0
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
-
-if (Intl.PluralRules == null) {
-  require('intl-pluralrules');
-}
