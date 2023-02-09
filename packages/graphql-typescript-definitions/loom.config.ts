4bee85ea3d0d3e76c51f22f6f54b1509df76ccce
Ben Scott <227292+BPScott@users.noreply.github.com>
2023-02-09 13:18:19 -0800

    Commit bin files instead of having loom generate them (#2558)

--- packages/graphql-typescript-definitions/loom.config.ts
+++ packages/graphql-typescript-definitions/loom.config.ts
@@ -4,6 +4,6 @@ import {quiltPackage} from '../../config/loom';
 
 export default createPackage((pkg) => {
   pkg.entry({root: './src/index.ts'});
-  pkg.binary({name: 'graphql-typescript-definitions', root: './src/cli.ts'});
+  pkg.entry({root: './src/cli.ts'});
   pkg.use(quiltPackage());
 });
