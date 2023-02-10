5c81d2ce05f446915a801c38d2e7cf798247adba
Ben Scott <ben.scott@shopify.com>
2023-02-09 19:59:11 -0800

    Revert changes to binary info in loom.config files
    This messed with the generation of the entrypoint files, so undo it.

--- packages/graphql-validate-fixtures/loom.config.ts
+++ packages/graphql-validate-fixtures/loom.config.ts
@@ -4,6 +4,6 @@ import {quiltPackage} from '../../config/loom';
 
 export default createPackage((pkg) => {
   pkg.entry({root: './src/index.ts'});
-  pkg.entry({root: './src/cli.ts'});
+  pkg.binary({name: 'graphql-validate-fixtures', root: './src/cli.ts'});
   pkg.use(quiltPackage());
 });
