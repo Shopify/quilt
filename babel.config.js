2781826fac6a036eeb5da2aba15ce54b21d03ca9
Ben Scott <227292+BPScott@users.noreply.github.com>
2023-03-03 12:54:51 -0800

    Deprecate and remove @shopify/decorators (#2581)
    * Replace usage of decorators with function-enhancers
    
    * Deprecate and remove @shopify/decorators
    
    * Remove transpilation of decorators

--- babel.config.js
+++ babel.config.js
@@ -13,28 +13,14 @@ module.exports = function (api) {
       ['@babel/preset-react', {development, useBuiltIns: true}],
     ],
     plugins: [
-      // class-properties and private-methods are handled by preset-env,
-      // however when using the legacy version of decorators - with TypeScript's
-      // experimentalDecorators option - you need to add
-      // them explicitly after proposal-decorators. Also the setPublicClassFields
-      // and privateFieldsAsProperties assumptions must be enabled (which is
-      // handled at the bottom of this function).
-      // We need to keep class-properties and private-methods around anyway even
-      // if we were to remove transpilation of legacy decorators, because they
-      // are not supported by webpack 4 because of missing support in acorn v6
-      // (support is in acorn v7 which is used by webpack 5).
-      ['@babel/plugin-proposal-decorators', {legacy: true}],
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-private-methods',
-
-      // nullish-coalescing, optional-chaining, and numeric separators,
-      // are handled by preset-env.
+      // These plugins are handled by preset-env.
       // But they aren't yet supported in webpack 4 because of missing support
       // in acorn v6 (support is in acorn v7, which is used in webpack v5).
       // So we want to always transpile this synax away
       // See https://github.com/webpack/webpack/issues/10227
-      // Can be removed once we drop support for webpack v4 (or these features
-      // are backported to acorn v6).
+      // Can be removed once we drop support for webpack v4
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
       '@babel/plugin-proposal-numeric-separator',
       '@babel/plugin-proposal-nullish-coalescing-operator',
       '@babel/plugin-proposal-optional-chaining',
