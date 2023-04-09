deae3f490c1247af83c393b47a77af4dc88d0282
HBS <30705623+MrHBS@users.noreply.github.com>
2023-04-09 07:19:58 +0300

    Delete packages/react-testing/src/tests/setup directory
    I believe this was added for loom but @BPScott nuked it.

--- packages/react-testing/src/tests/setup/environment.ts
+++
@@ -1,7 +0,0 @@
-// As of React 18, we must specifically tell React it is operating in a unit-test-like environment
-// This enables the use of act() and ensures that warnings will be logged on unwrapped updates
-// See: https://github.com/reactwg/react-18/discussions/102
-// See: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#configuring-your-testing-environment
-globalThis.IS_REACT_ACT_ENVIRONMENT = true;
-
-export {};
