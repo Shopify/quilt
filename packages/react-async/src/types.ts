a9bc620007a82d1b81f82b0eb23ed48f12583cb2
Sergey Melnikov <sergey.melnikov@shopify.com>
2023-09-01 13:20:12 -0400

    [react-async] Expose renderLoading on AsyncComponentType

--- packages/react-async/src/types.ts
+++ packages/react-async/src/types.ts
@@ -1,4 +1,4 @@
-import type {ReactElement} from 'react';
+import type {ReactElement, ReactNode} from 'react';
 import type {Resolver} from '@shopify/async';
 import type {IfAllOptionalKeys, NoInfer} from '@shopify/useful-types';
 
@@ -55,6 +55,7 @@ export interface AsyncComponentType<
   Preload(props: PreloadOptions): React.ReactElement<{}> | null;
   Prefetch(props: PrefetchOptions): React.ReactElement<{}> | null;
   KeepFresh(props: KeepFreshOptions): React.ReactElement<{}> | null;
+  renderLoading: ((props: Props) => ReactNode) | undefined;
 }
 
 export type PreloadOptions<T> = T extends AsyncHookTarget<
